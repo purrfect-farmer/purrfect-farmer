4730350417c1798c3b4a78ebb2341faf7c6bd0e7
Sadiq Salau <sadiqsalau888@gmail.com>
2024-11-14 20:38:14 +0100

    Added Not Pixel Secrets

--- src/drops/notpixel/components/NotPixelApp.jsx
+++ src/drops/notpixel/components/NotPixelApp.jsx
@@ -1,10 +1,13 @@
+import * as Tabs from "@radix-ui/react-tabs";
 import ReactTimeAgo from "react-time-ago";
 import Slider from "@/components/Slider";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import toast from "react-hot-toast";
 import useProcessLock from "@/hooks/useProcessLock";
+import useSocketDispatchCallback from "@/hooks/useSocketDispatchCallback";
 import useSocketState from "@/hooks/useSocketState";
+import useSocketTabs from "@/hooks/useSocketTabs";
 import { CgSpinner } from "react-icons/cg";
 import { cn, delayForSeconds } from "@/lib/utils";
 import { useEffect } from "react";
@@ -16,17 +19,47 @@ import useFarmerAutoProcess from "../hooks/useFarmerAutoProcess";
 import useNotPixelMiningClaimMutation from "../hooks/useNotPixelMiningClaimMutation";
 import useNotPixelMiningStatusQuery from "../hooks/useNotPixelMiningStatusQuery";
 import useNotPixelRepaintMutation from "../hooks/useNotPixelRepaintMutation";
+import useNotPixelSecretWordMutation from "../hooks/useNotSecretWordMutation";
 
 TimeAgo.addDefaultLocale(en);
 
 export default function NotPixelApp({ diff, updatedAt }) {
+  const tabs = useSocketTabs("notpixel.farmer-tabs", "paint");
   const miningQuery = useNotPixelMiningStatusQuery();
   const mining = miningQuery.data;
 
   const process = useProcessLock("notpixel.app");
 
+  const [secretWord, setSecretWord, dispatchAndSetSecretWord] = useSocketState(
+    "notpixel.secret-word",
+    ""
+  );
+
   const repaintMutation = useNotPixelRepaintMutation();
   const claimMiningMutation = useNotPixelMiningClaimMutation();
+  const secretWordMutation = useNotPixelSecretWordMutation();
+
+  /** Check Secret Word */
+  const [checkSecretWord, dispatchAndCheckSecretWord] =
+    useSocketDispatchCallback(
+      "notpixel.check-secret-word",
+      (word) => {
+        if (!word) return;
+        toast.promise(
+          secretWordMutation
+            .mutateAsync(word)
+            .then((data) =>
+              data.secretWord.success ? data : Promise.reject(data)
+            ),
+          {
+            loading: "Checking...",
+            success: "Success",
+            error: "Error...",
+          }
+        );
+      },
+      []
+    );
 
   const [farmingSpeed, , dispatchAndSetFarmingSpeed] = useSocketState(
     "notpixel.farming-speed",
@@ -152,89 +185,153 @@ export default function NotPixelApp({ diff, updatedAt }) {
           </h1>
           <h2 className="text-center">Charges: {mining.charges}</h2>
 
-          <button
-            onClick={() => process.dispatchAndToggle(!process.started)}
-            disabled={mining.charges < 1}
-            className={cn(
-              "text-white px-4 py-2 rounded-lg disabled:opacity-50",
-              !process.started ? "bg-black" : "bg-red-500"
-            )}
-          >
-            {!process.started ? "Start Farming" : "Stop Farming"}
-          </button>
-
-          {/* Farming Speed */}
-          <div className="flex flex-col gap-1">
-            {/* Speed Control */}
-            <Slider
-              value={[farmingSpeed]}
-              min={0}
-              max={15}
-              step={0.5}
-              onValueChange={([value]) =>
-                dispatchAndSetFarmingSpeed(Math.max(0.5, value))
-              }
-              trackClassName="bg-neutral-200"
-              rangeClassName="bg-neutral-900"
-              thumbClassName="bg-neutral-900"
-            />
-
-            {/* Speed Display */}
-            <div className="text-center">
-              Painting Speed: <span className="font-bold">{farmingSpeed}s</span>
-            </div>
-          </div>
-
-          {process.started ? (
-            <>
-              <div className="flex flex-col items-center justify-center gap-2 ">
-                {/* Color */}
-                {pixel ? (
-                  <>
-                    <div className="flex flex-col w-full gap-1 p-4 font-bold bg-black rounded-lg">
-                      <p className="text-blue-500">Pixel ID: {pixel.pixelId}</p>
-                      <p className="text-rose-500">Offset: {pixel.offset}</p>
-                      <p className="text-green-500">Color: {pixel.color}</p>
-                      <p className="text-pink-500">X: {pixel.x}</p>
-                      <p className="text-orange-500">Y: {pixel.y}</p>
-                      <p className="text-purple-500">
-                        Position-X: {pixel.positionX}
-                      </p>
-                      <p className="text-lime-500">
-                        Position-Y: {pixel.positionY}
-                      </p>
-                    </div>
-
-                    {/* Box Color */}
-                    <div
-                      className="w-full h-10 border rounded-lg"
-                      style={{ backgroundColor: pixel.color }}
-                    />
-                  </>
-                ) : null}
-
-                <div
+          <Tabs.Root {...tabs.root} className="flex flex-col gap-4">
+            <Tabs.List className="grid grid-cols-2">
+              {["paint", "secrets"].map((value, index) => (
+                <Tabs.Trigger
+                  key={index}
+                  value={value}
                   className={cn(
-                    "text-center font-bold",
-                    {
-                      pending: "text-orange-500",
-                      success: "text-green-500",
-                      error: "text-red-500",
-                    }[repaintMutation.status]
+                    "p-2",
+                    "border-b-4 border-transparent",
+                    "data-[state=active]:border-black"
                   )}
                 >
-                  {
-                    {
-                      idle: "Waiting...",
-                      pending: "Painting...",
-                      success: "Painted! Refetching...",
-                      error: "Error..",
-                    }[repaintMutation.status]
-                  }
+                  {value.toUpperCase()}
+                </Tabs.Trigger>
+              ))}
+            </Tabs.List>
+            <Tabs.Content
+              forceMount
+              className="data-[state=inactive]:hidden"
+              value="paint"
+            >
+              <div className="flex flex-col gap-2">
+                <button
+                  onClick={() => process.dispatchAndToggle(!process.started)}
+                  disabled={mining.charges < 1}
+                  className={cn(
+                    "text-white px-4 py-2 rounded-lg disabled:opacity-50",
+                    !process.started ? "bg-black" : "bg-red-500"
+                  )}
+                >
+                  {!process.started ? "Start Farming" : "Stop Farming"}
+                </button>
+
+                {/* Farming Speed */}
+                <div className="flex flex-col gap-1">
+                  {/* Speed Control */}
+                  <Slider
+                    value={[farmingSpeed]}
+                    min={0}
+                    max={15}
+                    step={0.5}
+                    onValueChange={([value]) =>
+                      dispatchAndSetFarmingSpeed(Math.max(0.5, value))
+                    }
+                    trackClassName="bg-neutral-200"
+                    rangeClassName="bg-neutral-900"
+                    thumbClassName="bg-neutral-900"
+                  />
+
+                  {/* Speed Display */}
+                  <div className="text-center">
+                    Painting Speed:{" "}
+                    <span className="font-bold">{farmingSpeed}s</span>
+                  </div>
                 </div>
+
+                {process.started ? (
+                  <>
+                    <div className="flex flex-col items-center justify-center gap-2 ">
+                      {/* Color */}
+                      {pixel ? (
+                        <>
+                          <div className="flex flex-col w-full gap-1 p-4 font-bold bg-black rounded-lg">
+                            <p className="text-blue-500">
+                              Pixel ID: {pixel.pixelId}
+                            </p>
+                            <p className="text-rose-500">
+                              Offset: {pixel.offset}
+                            </p>
+                            <p className="text-green-500">
+                              Color: {pixel.color}
+                            </p>
+                            <p className="text-pink-500">X: {pixel.x}</p>
+                            <p className="text-orange-500">Y: {pixel.y}</p>
+                            <p className="text-purple-500">
+                              Position-X: {pixel.positionX}
+                            </p>
+                            <p className="text-lime-500">
+                              Position-Y: {pixel.positionY}
+                            </p>
+                          </div>
+
+                          {/* Box Color */}
+                          <div
+                            className="w-full h-10 border rounded-lg"
+                            style={{ backgroundColor: pixel.color }}
+                          />
+                        </>
+                      ) : null}
+
+                      <div
+                        className={cn(
+                          "text-center font-bold",
+                          {
+                            pending: "text-orange-500",
+                            success: "text-green-500",
+                            error: "text-red-500",
+                          }[repaintMutation.status]
+                        )}
+                      >
+                        {
+                          {
+                            idle: "Waiting...",
+                            pending: "Painting...",
+                            success: "Painted! Refetching...",
+                            error: "Error..",
+                          }[repaintMutation.status]
+                        }
+                      </div>
+                    </div>
+                  </>
+                ) : null}
               </div>
-            </>
-          ) : null}
+            </Tabs.Content>
+            <Tabs.Content
+              forceMount
+              className="data-[state=inactive]:hidden"
+              value="secrets"
+            >
+              <div className="flex flex-col gap-2">
+                {/* Secret Word Input */}
+                <input
+                  className={cn(
+                    "outline-0",
+                    "rounded-lg",
+                    "border border-black",
+                    "focus:ring focus:ring-neutral-500",
+                    "placeholder:text-neutral-600",
+                    "px-4 py-2"
+                  )}
+                  onChange={(ev) => dispatchAndSetSecretWord(ev.target.value)}
+                  value={secretWord}
+                  placeholder="Secret Word"
+                />
+                {/* Check Button */}
+                <button
+                  className={cn(
+                    "text-white px-4 py-2 rounded-lg disabled:opacity-50",
+                    "bg-black"
+                  )}
+                  onClick={() => dispatchAndCheckSecretWord(secretWord)}
+                >
+                  Check
+                </button>
+              </div>
+            </Tabs.Content>
+          </Tabs.Root>
         </>
       ) : (
         <CgSpinner className="w-5 h-5 mx-auto animate-spin" />
