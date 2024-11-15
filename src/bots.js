28ffc8ecafdc7b8857801f7068b1363b82bbd062
Sadiq Salau <sadiqsalau888@gmail.com>
2024-11-15 12:54:59 +0100

    Added NotGram Bot

--- src/bots.js
+++ src/bots.js
@@ -18,6 +18,11 @@ export const botIcons = Object.fromEntries(
 
 const bots = [
   {
+    title: "Notgram",
+    icon: botIcons["notgram"],
+    telegramLink: "https://t.me/notgram_game_bot?start=r1147265290",
+  },
+  {
     title: "Paws",
     icon: botIcons["paws"],
     telegramLink: "https://t.me/PAWSOG_bot/PAWS?startapp=5MwPEyav",
