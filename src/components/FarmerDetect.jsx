68c1cc93c50324a2de5f0c7618c8498f1a0b6bd8
Sadiq Salau <sadiqsalau888@gmail.com>
2024-11-16 11:41:41 +0100

    Changed Detecting Auth to Preparing Farmer

--- src/components/FarmerDetect.jsx
+++ src/components/FarmerDetect.jsx
@@ -5,7 +5,7 @@ export default function FarmerDetect({ status, title, icon, className }) {
     <div className="flex flex-col items-center justify-center min-w-0 min-h-0 gap-2 p-4 grow">
       <img src={icon} alt={title} className="w-16 h-16 my-2 rounded-full" />
       <h3 className="font-bold text-center">
-        {status === "pending-webapp" ? "Getting App" : "Detecting Auth"}
+        {status === "pending-webapp" ? "Getting App" : "Preparing Farmer"}
       </h3>
       <p className={cn("text-center text-neutral-500", className)}>
         {status === "pending-webapp" ? (
