37f7b8c32d863ac3918c0331ecf0ab5010ce1202
Sadiq Salau <sadiqsalau888@gmail.com>
2024-12-02 10:06:19 +0100

    Removed Goats Farmer

--- src/drops/goats/components/GoatsAuthDetect.jsx
+++
@@ -1,9 +0,0 @@
-import FarmerDetect from "@/components/FarmerDetect";
-
-import GoatsIcon from "../assets/images/icon.png?format=webp&w=80";
-
-export default function GoatsAuthDetect({ status }) {
-  return (
-    <FarmerDetect title={"Goats Farmer"} icon={GoatsIcon} status={status} />
-  );
-}
