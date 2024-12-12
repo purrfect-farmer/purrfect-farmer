4685d99058fe53c0316255d7f9447fa60671fa75
Sadiq Salau <sadiqsalau888@gmail.com>
2024-12-12 01:42:58 +0100

    Display XP and Level

--- src/drops/dreamcoin/components/DreamCoinInfoDisplay.jsx
+++ src/drops/dreamcoin/components/DreamCoinInfoDisplay.jsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import CoinIcon from "../assets/images/coin.png?format=webp&w=80";
 import EnergyIcon from "../assets/images/energy.png?format=webp&w=80";
 import useDreamCoinUserQuery from "../hooks/useDreamCoinUserQuery";
@@ -19,6 +20,11 @@ export default function DreamCoinInfoDisplay() {
             {user?.name}
           </h2>
 
+          {/* Level */}
+          <h2 className="font-bold text-center text-orange-500">
+            LVL {user?.level?.current || 0}
+          </h2>
+
           {/* Balance */}
           <h3 className="text-2xl font-bold text-center text-orange-500">
             <img src={CoinIcon} className="inline w-5 h-5" />{" "}
@@ -30,6 +36,29 @@ export default function DreamCoinInfoDisplay() {
             <img src={EnergyIcon} className="inline w-5" />{" "}
             {user?.energy?.current} / {user?.energy?.max}
           </h4>
+
+          {/* Airdrop */}
+          <div className="flex flex-col items-center justify-center gap-1 text-center whitespace-nowrap">
+            <h4
+              className={cn(
+                "relative px-4 py-2 rounded-full",
+                "bg-green-600 text-white",
+                "font-bold text-sm"
+              )}
+            >
+              {Intl.NumberFormat().format(user?.airDropXp || 0)} XP
+            </h4>
+            <h4
+              className={cn(
+                "text-xs",
+                "px-2 py-1 rounded-full",
+                "text-green-100 bg-green-800",
+                "font-bold"
+              )}
+            >
+              +{Intl.NumberFormat().format(user?.airDropXpPerHour || 0)} XP/H
+            </h4>
+          </div>
         </div>
       )}
     </>
