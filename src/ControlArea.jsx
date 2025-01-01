c4f9331156dc2b9ba6a4b4f0d831754d33cd9e3e
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-01 21:10:24 +0100

    Display Zoomies Task

--- src/ControlArea.jsx
+++ src/ControlArea.jsx
@@ -23,70 +23,91 @@ export default memo(function ControlArea() {
 
       {/* Zoomies Control */}
       {zoomies.enabled && zoomies.current.drop ? (
-        <div
-          className={cn(
-            "flex items-center gap-2 px-2 shrink-0",
-            "border-t dark:border-neutral-700"
-          )}
-        >
-          <button
-            className="relative shrink-0"
-            onClick={() => setActiveTab(zoomies.current.drop.id)}
-          >
-            <img
-              src={zoomies.current.drop.icon}
-              className="w-8 h-8 rounded-full shrink-0"
-            />
-          </button>
-          <div className="flex flex-col min-w-0 min-h-0 truncate grow">
-            <h4 className="font-bold">{zoomies.current.drop.title}</h4>
-            <p className="text-neutral-400">
-              Running{" "}
-              {zoomies.current.drop.tasks.indexOf(zoomies.current.task) + 1} of{" "}
-              {zoomies.current.drop.tasks.length}
-            </p>
-          </div>
-
-          {/* Refresh Button */}
-          <button
-            className="flex items-center justify-center w-10 h-10 shrink-0"
-            onClick={() => zoomies.dispatchAndRefresh()}
-          >
-            <HiOutlineArrowPath className="w-6 h-6 text-orange-500" />
-          </button>
-
-          {/* Backward Button */}
-          <button
-            className="flex items-center justify-center w-10 h-10 shrink-0"
-            onClick={() => zoomies.processPreviousTask()}
-          >
-            <HiOutlineBackward className="w-6 h-6 text-orange-500" />
-          </button>
-
-          {/* Forward Button */}
-          <button
-            className="flex items-center justify-center w-10 h-10 shrink-0"
-            onClick={() => zoomies.processNextTask()}
-          >
-            <HiOutlineForward className="w-6 h-6 text-orange-500" />
-          </button>
-
-          {/* Cycles */}
-          <span
+        <>
+          <div
             className={cn(
-              "shrink-0",
-              "bg-gradient-to-br",
-              "from-red-500 to-purple-500",
-              "text-white font-bold",
-              "rounded-full",
-              "text-xs",
-              "flex items-center justify-center",
-              "w-5 h-5"
+              "flex items-center gap-2 px-2 py-1",
+              "text-white bg-purple-500 font-bold"
             )}
           >
-            {zoomies.current.cycles > 9 ? "9+" : zoomies.current.cycles}
-          </span>
-        </div>
+            <span
+              className={cn(
+                "flex items-center justify-center",
+                "w-6 h-6 text-xs",
+                "rounded-full shrink-0",
+                "bg-purple-600"
+              )}
+            >
+              {zoomies.current.drop.tasks.indexOf(zoomies.current.task) + 1}
+            </span>
+            <h1 className="min-w-0 min-h-0 truncate grow">
+              {zoomies.current.task.toUpperCase()}
+            </h1>
+          </div>
+          <div
+            className={cn(
+              "flex items-center gap-2 px-2 shrink-0",
+              "border-t dark:border-neutral-700"
+            )}
+          >
+            <button
+              className="relative shrink-0"
+              onClick={() => setActiveTab(zoomies.current.drop.id)}
+            >
+              <img
+                src={zoomies.current.drop.icon}
+                className="w-8 h-8 rounded-full shrink-0"
+              />
+            </button>
+            <div className="flex flex-col min-w-0 min-h-0 truncate grow">
+              <h4 className="font-bold">{zoomies.current.drop.title}</h4>
+              <p className="text-neutral-400">
+                {zoomies.current.drop.tasks.indexOf(zoomies.current.task) + 1}{" "}
+                of {zoomies.current.drop.tasks.length}
+              </p>
+            </div>
+
+            {/* Refresh Button */}
+            <button
+              className="flex items-center justify-center w-10 h-10 shrink-0"
+              onClick={() => zoomies.dispatchAndRefresh()}
+            >
+              <HiOutlineArrowPath className="w-6 h-6 text-orange-500" />
+            </button>
+
+            {/* Backward Button */}
+            <button
+              className="flex items-center justify-center w-10 h-10 shrink-0"
+              onClick={() => zoomies.processPreviousTask()}
+            >
+              <HiOutlineBackward className="w-6 h-6 text-orange-500" />
+            </button>
+
+            {/* Forward Button */}
+            <button
+              className="flex items-center justify-center w-10 h-10 shrink-0"
+              onClick={() => zoomies.processNextTask()}
+            >
+              <HiOutlineForward className="w-6 h-6 text-orange-500" />
+            </button>
+
+            {/* Cycles */}
+            <span
+              className={cn(
+                "shrink-0",
+                "bg-gradient-to-br",
+                "from-red-500 to-purple-500",
+                "text-white font-bold",
+                "rounded-full",
+                "text-xs",
+                "flex items-center justify-center",
+                "w-5 h-5"
+              )}
+            >
+              {zoomies.current.cycles > 9 ? "9+" : zoomies.current.cycles}
+            </span>
+          </div>
+        </>
       ) : null}
 
       {/* Utils Control */}
