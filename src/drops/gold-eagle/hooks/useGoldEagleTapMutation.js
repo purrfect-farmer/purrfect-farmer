4c1eb2f179a3e6ac63fa8b78b46865a5cefbd43b
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-18 19:50:57 +0100

    Added Nonce Calculation

--- src/drops/gold-eagle/hooks/useGoldEagleTapMutation.js
+++ src/drops/gold-eagle/hooks/useGoldEagleTapMutation.js
@@ -1,19 +1,24 @@
 import useFarmerApi from "@/hooks/useFarmerApi";
 import { useMutation } from "@tanstack/react-query";
 import { uuid } from "@/lib/utils";
+import { totp } from "otplib";
 
-export default function useGoldEagleTapMutation() {
+export default function useGoldEagleTapMutation(hex) {
   const api = useFarmerApi();
   return useMutation({
     mutationKey: ["gold-eagle", "tap"],
-    mutationFn: ({ taps, available }) =>
-      api
+    mutationFn: ({ taps, available }) => {
+      const nonce = btoa(totp.generate(hex));
+
+      return api
         .post("https://gold-eagle-api.fly.dev/tap", {
+          nonce,
           ["count"]: taps,
           ["available_taps"]: available,
           ["timestamp"]: Math.floor(Date.now() / 1000),
           salt: uuid(),
         })
-        .then((res) => res.data),
+        .then((res) => res.data);
+    },
   });
 }
--- src/drops/gold-eagle/lib/utils.js
+++ src/drops/gold-eagle/lib/utils.js
@@ -1,15 +1,35 @@
+import { Buffer } from "buffer";
+import { HashAlgorithms, KeyEncodings } from "@otplib/core";
+import { createDigest } from "@otplib/plugin-crypto-js";
+import { decode } from "hi-base32";
 import { getDropMainScript } from "@/lib/utils";
+import { totp } from "otplib";
+
+window.Buffer = window.Buffer || Buffer;
+totp.options = {
+  createDigest,
+  encoding: KeyEncodings.HEX,
+  digits: 6,
+  step: 2,
+  algorithm: HashAlgorithms.SHA1,
+};
 
 export async function getGoldEagleGame() {
   if (getGoldEagleGame.DATA) return getGoldEagleGame.DATA;
   const url = "https://telegram.geagle.online";
   const scriptResponse = await getDropMainScript(url);
 
-  const tapSecret = scriptResponse.match(/TAP_SECRET="([^"]+)"/);
-
-  if (tapSecret) {
+  const match = scriptResponse.match(/TAP_SECRET="([^"]+)"/);
+  if (match) {
+    const secret = match[1];
+    const bytes = decode.asBytes(secret);
+    const hex = Buffer.from(bytes).toString("hex");
+    const initialNonce = btoa(totp.generate(hex));
     const result = {
-      tapSecret: tapSecret[1],
+      initialNonce,
+      secret,
+      bytes,
+      hex,
     };
 
     return (getGoldEagleGame.DATA = result);
