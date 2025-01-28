1c95d5fbeb27b3c251abc53550b1eb94dfbb18cc
Sadiq Salau <sadiqsalau888@gmail.com>
2025-01-28 18:55:57 +0100

    Updated Gold Eagle Script

--- src/drops/gold-eagle/lib/utils.js
+++ src/drops/gold-eagle/lib/utils.js
@@ -6,7 +6,7 @@ import { decode } from "hi-base32";
 import { findDropMainScript } from "@/lib/utils";
 import { totp } from "otplib";
 
-const INDEX_SCRIPT = "index-CstGB8vG";
+const INDEX_SCRIPT = "index-DygVGXdT";
 const SECRET = "FZYQHANLB3I2KAWEOKI4T2PVXHHZ4K5F";
 const ENCRYPTION_ALGORITHM = "RSAES-PKCS1-V1_5";
 const PEM =
