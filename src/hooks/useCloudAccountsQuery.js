06ad18a90c66354aa6aafa79f5e658ede1d521b6
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-10 20:12:45 +0100

    Refetch Accounts

--- src/hooks/useCloudAccountsQuery.js
+++ src/hooks/useCloudAccountsQuery.js
@@ -6,6 +6,7 @@ export default function useCloudAccountsQuery() {
   const { cloudBackend } = useCloudContext();
 
   return useQuery({
+    refetchInterval: 10000,
     queryKey: ["core", "cloud", "accounts"],
     queryFn: ({ signal }) =>
       cloudBackend.get("/api/accounts", { signal }).then((res) => res.data),
