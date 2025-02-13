31fa7fab046d5d6b04c82dece9553e7ecf9ffbb2
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-13 08:37:07 +0100

    Updated Cloud Manager

--- src/cloud/CloudAccounts.jsx
+++ src/cloud/CloudAccounts.jsx
@@ -1,12 +1,13 @@
-import useCloudAccountsQuery from "@/hooks/useCloudAccountsQuery";
-import { cn } from "@/lib/utils";
-import { useMemo } from "react";
 import FunaticIcon from "@/drops/funatic/assets/images/icon.png?format=webp&w=80";
 import GoldEagleIcon from "@/drops/gold-eagle/assets/images/icon.png?format=webp&w=80";
-import { HiOutlineXMark } from "react-icons/hi2";
-import useCloudDisconnectAccountMutation from "@/hooks/useCloudDisconnectAccountMutation";
-import { useCallback } from "react";
+import Input from "@/components/Input";
 import toast from "react-hot-toast";
+import useCloudAccountsQuery from "@/hooks/useCloudAccountsQuery";
+import useCloudDisconnectAccountMutation from "@/hooks/useCloudDisconnectAccountMutation";
+import { HiOutlineXMark } from "react-icons/hi2";
+import { cn } from "@/lib/utils";
+import { useCallback } from "react";
+import { useMemo, useState } from "react";
 
 const CLOUD_FARMERS = {
   funatic: {
@@ -20,6 +21,7 @@ const CLOUD_FARMERS = {
 };
 
 export default function CloudAccounts() {
+  const [search, setSearch] = useState("");
   const disconnectAccountMutation = useCloudDisconnectAccountMutation();
   const accountsQuery = useCloudAccountsQuery();
 
@@ -27,11 +29,18 @@ export default function CloudAccounts() {
     () =>
       accountsQuery.data
         ? Object.entries(accountsQuery.data).map(([k, v]) => ({
-            id: k,
             ...v,
+            id: k,
+            users: search
+              ? v.users.filter(
+                  (user) =>
+                    user.username.includes(search) ||
+                    user["user_id"].toString().includes(search)
+                )
+              : v.users,
           }))
         : [],
-    [accountsQuery.data]
+    [search, accountsQuery.data]
   );
 
   const disconnectAccount = useCallback(
@@ -57,55 +66,67 @@ export default function CloudAccounts() {
   ) : accountsQuery.isError ? (
     <p className="text-center text-red-500">Error...</p>
   ) : (
-    <div className="flex flex-col gap-8">
-      {groups.map((group) => (
-        <div key={group.id} className="flex flex-col gap-2">
-          <h3 className="flex items-center gap-2 px-2">
-            <img
-              src={CLOUD_FARMERS[group.id].icon}
-              className="w-6 h-6 rounded-full shrink-0"
-            />
-            <span className="grow">{CLOUD_FARMERS[group.id].title}</span>
-            <span className="px-2 py-px text-xs text-white bg-purple-500 rounded-full shrink-0">
-              {group.total}
-            </span>
-          </h3>
-          {group.users.length ? (
-            group.users.map((account) => (
-              <div key={account.id} className="flex gap-2">
-                {/* Details */}
-                <div
-                  className={cn(
-                    "flex items-center min-w-0 min-h-0",
-                    "gap-2 p-2 rounded-lg grow bg-neutral-100 dark:bg-neutral-700"
-                  )}
-                >
-                  <img
-                    src={account["photo_url"]}
-                    className="w-6 h-6 rounded-full shrink-0"
-                  />{" "}
-                  <h5>{account.username || account["user_id"]}</h5>
-                </div>
+    <div className="flex flex-col gap-4">
+      {/* Search */}
+      <Input
+        placeholder="Search"
+        value={search}
+        onChange={(ev) => setSearch(ev.target.value)}
+      />
 
-                {/* Terminate Button */}
-                <button
-                  title="Disconnect Account"
-                  onClick={() => disconnectAccount(account.id)}
-                  className={cn(
-                    "text-red-600 bg-red-100",
-                    "dark:text-white dark:bg-red-500",
-                    "px-3 rounded-lg shrink-0"
-                  )}
-                >
-                  <HiOutlineXMark className="w-4 h-4" />
-                </button>
-              </div>
-            ))
-          ) : (
-            <p className="text-center">No account to display..</p>
-          )}
-        </div>
-      ))}
+      {/* Accounts */}
+      <div className="flex flex-col gap-8">
+        {groups.map((group) => (
+          <div key={group.id} className="flex flex-col gap-2">
+            <h3 className="flex items-center gap-2 px-2">
+              <img
+                src={CLOUD_FARMERS[group.id].icon}
+                className="w-6 h-6 rounded-full shrink-0"
+              />
+              <span className="font-bold grow">
+                {CLOUD_FARMERS[group.id].title}
+              </span>
+              <span className="px-2 py-px text-xs text-white bg-purple-500 rounded-full shrink-0">
+                {group.users.length}
+              </span>
+            </h3>
+            {group.users.length ? (
+              group.users.map((account) => (
+                <div key={account.id} className="flex gap-2">
+                  {/* Details */}
+                  <div
+                    className={cn(
+                      "flex items-center min-w-0 min-h-0",
+                      "gap-2 p-2 rounded-lg grow bg-neutral-100 dark:bg-neutral-700"
+                    )}
+                  >
+                    <img
+                      src={account["photo_url"]}
+                      className="w-6 h-6 rounded-full shrink-0"
+                    />{" "}
+                    <h5>{account.username || account["user_id"]}</h5>
+                  </div>
+
+                  {/* Terminate Button */}
+                  <button
+                    title="Disconnect Account"
+                    onClick={() => disconnectAccount(account.id)}
+                    className={cn(
+                      "text-red-600 bg-red-100",
+                      "dark:text-red-500 dark:bg-neutral-700",
+                      "px-3 rounded-lg shrink-0"
+                    )}
+                  >
+                    <HiOutlineXMark className="w-5 h-5" />
+                  </button>
+                </div>
+              ))
+            ) : (
+              <p className="text-center">No account to display..</p>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
