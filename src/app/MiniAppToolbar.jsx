9722542966fa680a4c15913ec0f6269e220cec47
Sadiq Salau <sadiqsalau888@gmail.com>
2025-02-23 10:18:25 +0100

    Updated Toolbar

--- src/app/MiniAppToolbar.jsx
+++ src/app/MiniAppToolbar.jsx
@@ -1,40 +1,67 @@
+import AppIcon from "@/assets/images/icon.png?inline&format=webp&w=64";
+import Draggable from "react-draggable";
 import copy from "copy-to-clipboard";
 import styled from "styled-components";
+import { AiOutlineDrag } from "react-icons/ai";
 import {
   HiOutlineArrowTopRightOnSquare,
   HiOutlineClipboard,
 } from "react-icons/hi2";
 import { useCallback } from "react";
+import { useRef } from "react";
 
-const Container = styled.div`
-  background-color: oklch(0.496 0.265 301.924);
-  color: white;
+const Wrapper = styled.div`
   position: fixed;
-  bottom: 0;
+  bottom: 12px;
   left: 0;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 8px;
-  padding: 8px;
   font-size: 12px;
   z-index: 99999;
 `;
 
-const Button = styled.button`
-  background-color: oklch(0.627 0.265 303.9);
+const Container = styled.div`
+  background-color: black;
   color: white;
-  border-radius: 100px;
   display: flex;
   align-items: center;
-  gap: 8px;
-  padding: 8px 16px;
+  justify-content: center;
+  padding: 6px;
+  gap: 6px;
+  font-size: 12px;
+  z-index: 99999;
+  border-radius: 999px;
+  box-sizing: border-box;
+`;
+
+const Image = styled.img`
+  border-radius: 999px;
+  width: 32px;
+  height: 32px;
+`;
+
+const Button = styled.button`
+  box-sizing: border-box;
+  background-color: #262626;
+  color: white;
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
   border: 0px;
   outline: 0px;
   cursor: pointer;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+  &:hover {
+    background-color: oklch(0.768 0.233 130.85);
+    color: black;
+  }
 `;
 
 const OpenURLIcon = styled(HiOutlineArrowTopRightOnSquare)`
@@ -47,7 +74,15 @@ const ClipboardIcon = styled(HiOutlineClipboard)`
   height: 16px;
 `;
 
+const HandleIcon = styled(AiOutlineDrag)`
+  width: 16px;
+  height: 16px;
+`;
+
 export default function MiniAppToolbar({ url }) {
+  const dragHandleClass = "draggable-handle";
+  const nodeRef = useRef(null);
+
   const openURL = useCallback(() => {
     window.open(url);
   }, [url]);
@@ -55,16 +90,31 @@ export default function MiniAppToolbar({ url }) {
   const copyURL = useCallback(() => {
     copy(url);
   }, [url]);
+
   return (
     <>
-      <Container>
-        <Button onClick={openURL}>
-          <OpenURLIcon /> Open URL
-        </Button>
-        <Button onClick={copyURL}>
-          <ClipboardIcon /> Copy URL
-        </Button>
-      </Container>
+      <Wrapper>
+        <Draggable handle={`.${dragHandleClass}`} nodeRef={nodeRef}>
+          <Container ref={nodeRef}>
+            <Image src={AppIcon} />
+
+            {/* Open */}
+            <Button onClick={openURL} title="Open URL">
+              <OpenURLIcon />
+            </Button>
+
+            {/* Copy */}
+            <Button onClick={copyURL} title="Copy URL">
+              <ClipboardIcon />
+            </Button>
+
+            {/* Drag */}
+            <Button className={dragHandleClass} title="Drag">
+              <HandleIcon />
+            </Button>
+          </Container>
+        </Draggable>
+      </Wrapper>
     </>
   );
 }
