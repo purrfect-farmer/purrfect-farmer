d88338377be4666099aa7307332e53fe6c2aa0a1
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-01 08:56:39 +0100

    Reduced Deps

--- src/drops/gold-eagle/lib/utils.js
+++ src/drops/gold-eagle/lib/utils.js
@@ -1,5 +1,4 @@
 import forge from "node-forge";
-import { Buffer } from "buffer";
 import { TOTP } from "totp-generator";
 import { fetchContent, findDropMainScript } from "@/lib/utils";
 
@@ -9,8 +8,8 @@ const ENCRYPTION_ALGORITHM = "RSAES-PKCS1-V1_5";
 const PUBLIC_KEY =
   "-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyH0A/d/2Dc1QGDCpVgD/ 8Xx1o3GHccjybtK3AM4Wv0faLZL6J1jDLGdmOEnE2+HkTuxTBSVBZT1a+8Iazxkd LqTihCZxGUxp6i9CZatICimC7LbdGJW++t+X9l7EH6uEBPuSjQcuNuaODQkefncW //rni5iksdd3pjQRLM+PVEMzPw+pvgfPfAn0fUDqer0itUJFQ5P0+tVaL/6AlcBY EqnirvIo8tfps/+9yGqc2znCVWwaR+1uCeVZ6gbt96XPVxaGf+hKn+TwiJo2sykH OGADDSK8sEWca7DqSQScGSTc5/DD2CeSK78pwlhYOQb6694PI0Cr5g+tpPm94gk/ nwIDAQAB -----END PUBLIC KEY-----";
 
-/** Expose Buffer */
-window.Buffer = window.Buffer || Buffer;
+/** Encryption Instance */
+const ENCRYPTION = forge.pki.publicKeyFromPem(PUBLIC_KEY);
 
 export async function getGoldEagleGame() {
   if (getGoldEagleGame.DATA) return getGoldEagleGame.DATA;
@@ -51,10 +50,8 @@ export function calculateGoldEagleData(taps, currentNonce) {
     ct: nonce,
   };
   const json = JSON.stringify(input);
+  const data = ENCRYPTION.encrypt(json, ENCRYPTION_ALGORITHM);
+  const result = btoa(data);
 
-  const buffer = Buffer.from(json, "utf8");
-  const key = forge.pki.publicKeyFromPem(PUBLIC_KEY);
-  const data = key.encrypt(buffer.toString("binary"), ENCRYPTION_ALGORITHM);
-
-  return Buffer.from(data, "binary").toString("base64");
+  return result;
 }
