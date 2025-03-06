aa5073c4b943292f9fbd174cfa304b62876c7b30
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-06 15:13:48 +0100

    Display Toolbar for all pages

--- src/extension/mini-app-toolbar-isolated.js
+++ src/extension/mini-app-toolbar-isolated.js
@@ -6,7 +6,7 @@ import { getSettings } from "@/lib/utils";
 /** Initial Location */
 const INITIAL_LOCATION = location.href;
 
-if (location.hash.includes("tgWebAppData") && window.parent !== window) {
+if (location.hash.includes("tgWebAppData")) {
   /** Initial State */
   let toolbar = null;
 
