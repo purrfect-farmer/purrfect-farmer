76a76e8c3b8ff22d55a5277f591dece4f8259858
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-05 15:26:44 +0100

    [FIX] Missing Import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import "@/lib/polyfills";
 
-import "./telegramWebAppEvents";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
