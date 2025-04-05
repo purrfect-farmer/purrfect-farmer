4cb3d0c3372f641f9f5630e23c3f4323f322976b
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-05 15:40:18 +0100

    [FIX] Unknown attributes

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,46 +2,51 @@ import { Slider as SliderPrimitive } from "radix-ui";
 import { cn } from "@/lib/utils";
 import { forwardRef, memo } from "react";
 
-const Slider = forwardRef((props, forwardedRef) => {
-  const value = props.value || props.defaultValue;
+const Slider = forwardRef(
+  (
+    { trackClassName, rangeClassName, thumbClassName, ...props },
+    forwardedRef
+  ) => {
+    const value = props.value || props.defaultValue;
 
-  return (
-    <SliderPrimitive.Slider
-      {...props}
-      ref={forwardedRef}
-      className={cn(
-        "relative flex items-center select-none touch-none",
-        "w-full h-6"
-      )}
-    >
-      <SliderPrimitive.Track
+    return (
+      <SliderPrimitive.Slider
+        {...props}
+        ref={forwardedRef}
         className={cn(
-          "relative h-2 bg-blue-200 rounded-full grow",
-          props.trackClassName
+          "relative flex items-center select-none touch-none",
+          "w-full h-6"
         )}
       >
-        <SliderPrimitive.Range
+        <SliderPrimitive.Track
           className={cn(
-            "absolute h-full bg-blue-500 rounded-full",
-            props.rangeClassName
+            "relative h-2 bg-blue-200 rounded-full grow",
+            trackClassName
           )}
-        />
-      </SliderPrimitive.Track>
-      {value.map((_, i) => (
-        <SliderPrimitive.Thumb
-          key={i}
-          className={cn(
-            "relative size-6 rounded-full",
-            "flex items-center justify-center text-xs",
-            "bg-blue-500 shadow-xs",
-            "border-4 border-white",
-            "focus:outline-hidden",
-            props.thumbClassName
-          )}
-        />
-      ))}
-    </SliderPrimitive.Slider>
-  );
-});
+        >
+          <SliderPrimitive.Range
+            className={cn(
+              "absolute h-full bg-blue-500 rounded-full",
+              rangeClassName
+            )}
+          />
+        </SliderPrimitive.Track>
+        {value.map((_, i) => (
+          <SliderPrimitive.Thumb
+            key={i}
+            className={cn(
+              "relative size-6 rounded-full",
+              "flex items-center justify-center text-xs",
+              "bg-blue-500 shadow-xs",
+              "border-4 border-white",
+              "focus:outline-hidden",
+              thumbClassName
+            )}
+          />
+        ))}
+      </SliderPrimitive.Slider>
+    );
+  }
+);
 
 export default memo(Slider);
