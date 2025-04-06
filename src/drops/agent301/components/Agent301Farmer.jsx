8e803baff4ed429d100df6b1bb486ea70471efe7
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-06 16:11:43 +0100

    Removed Gaps

--- src/drops/agent301/components/Agent301Farmer.jsx
+++ src/drops/agent301/components/Agent301Farmer.jsx
@@ -1,13 +1,14 @@
+import Tabs from "@/components/Tabs";
 import useFarmerAutoTab from "@/hooks/useFarmerAutoTab";
 import useMirroredTabs from "@/hooks/useMirroredTabs";
 import { memo } from "react";
+
 import Agent301BalanceDisplay from "./Agent301BalanceDisplay";
 import Agent301Icon from "../assets/images/icon.png?format=webp&w=80";
 import Agent301Lottery from "./Agent301Lottery";
 import Agent301Puzzle from "./Agent301Puzzle";
 import Agent301Tasks from "./Agent301Tasks";
 import Agent301Wheel from "./Agent301Wheel";
-import Tabs from "@/components/Tabs";
 
 export default memo(function Agent301Farmer() {
   const tabs = useMirroredTabs("agent301.farmer-tabs", [
@@ -35,7 +36,7 @@ export default memo(function Agent301Farmer() {
 
       <Tabs
         tabs={tabs}
-        rootClassName={"gap-4"}
+        rootClassName={"gap-0"}
         triggerClassName={"data-[state=active]:border-white"}
       >
         <Tabs.Content value="tickets">
