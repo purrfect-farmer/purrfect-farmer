206756fbd366e4259928682fd1a7284b7b3a4329
Sadiq Salau <sadiqsalau888@gmail.com>
2025-04-23 17:35:15 +0100

    Updated Permissions

--- src/app/Browser.jsx
+++ src/app/Browser.jsx
@@ -6,7 +6,7 @@ export default memo(function Browser({ url }) {
       src={url}
       className="w-full h-full border-0 outline-0"
       referrerPolicy="no-referrer"
-      allow="geolocation; microphone; accelerometer; camera; fullscreen; payment"
+      allow="camera; microphone; geolocation; fullscreen; autoplay; clipboard-write; encrypted-media; accelerometer; gyroscope; magnetometer; midi; payment; usb; vr; xr-spatial-tracking; picture-in-picture; screen-wake-lock"
     />
   );
 });
