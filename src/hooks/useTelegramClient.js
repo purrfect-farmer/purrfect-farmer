03af19658276fe4fc52d1c0ee9e1879143374ff9
Sadiq Salau <sadiqsalau888@gmail.com>
2025-05-31 11:30:01 +0100

    Refactored Telegram Client

--- src/hooks/useTelegramClient.js
+++ src/hooks/useTelegramClient.js
@@ -1,8 +1,6 @@
-import TelegramWebClient from "@/lib/TelegramWebClient";
-import TelegramWorkerClient from "@/lib/TelegramWorkerClient";
 import { createTelegramClient } from "@/lib/createTelegramClient";
 import { customLogger } from "@/lib/utils";
-import { useCallback, useLayoutEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 
@@ -12,47 +10,6 @@ export default function useTelegramClient(mode, session) {
   const ref = useRef(null);
   const hasSession = Boolean(session);
   const [connected, setConnected] = useState(false);
-
-  const execute = useCallback(
-    /**
-     * Executes a callback with the Telegram Client instance.
-     *
-     * @param {(client: TelegramWebClient | TelegramWorkerClient) => any} callback - The function to execute with the client.
-     * @returns {Promise<any>} The result of the callback.
-     */
-    async (callback) => {
-      if (ref.current === null) {
-        throw new Error("No Telegram Client!");
-      }
-
-      return callback(ref.current);
-    },
-    []
-  );
-
-  /** Start Bot from Link */
-  const startBotFromLink = useCallback(
-    (options) => execute((client) => client.startBotFromLink(options)),
-    [execute]
-  );
-
-  /** Get Webview */
-  const getWebview = useCallback(
-    (link) => execute((client) => client.getWebview(link)),
-    [execute]
-  );
-
-  /** Get Telegram WebApp */
-  const getTelegramWebApp = useCallback(
-    (link) => execute((client) => client.getTelegramWebApp(link)),
-    [execute]
-  );
-
-  /** Join Telegram Link */
-  const joinTelegramLink = useCallback(
-    (link) => execute((client) => client.joinTelegramLink(link)),
-    [execute]
-  );
 
   /** Initiate Client */
   useLayoutEffect(() => {
@@ -72,9 +29,6 @@ export default function useTelegramClient(mode, session) {
       /** Set Ref */
       ref.current = client;
 
-      /** Set Connection State */
-      setConnected(client.connected);
-
       return () => {
         client?.destroy();
         ref.current = null;
@@ -87,10 +41,5 @@ export default function useTelegramClient(mode, session) {
     ref,
     hasSession,
     connected,
-    execute,
-    getWebview,
-    getTelegramWebApp,
-    joinTelegramLink,
-    startBotFromLink,
   });
 }
