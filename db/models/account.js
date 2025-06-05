e6bdf89263e0c245616009628afbf01bd069d3be
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-05 09:15:16 +0100

    Set Active to true

--- db/models/account.js
+++ db/models/account.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
             required,
             association: "subscriptions",
             where: {
-              status: "active",
+              active: true,
             },
           },
         ],
