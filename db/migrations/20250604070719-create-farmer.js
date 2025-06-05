1817ff8cfb2b1ab3a87ee4bef928984e64d5177e
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-05 09:13:32 +0100

    Updated Field Name

--- db/migrations/20250604070719-create-farmer.js
+++ db/migrations/20250604070719-create-farmer.js
@@ -26,7 +26,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      telegramInitData: {
+      initData: {
         type: Sequelize.STRING,
       },
       headers: {
