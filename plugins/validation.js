6a68719dbf975400dbfc181510ed37514e0d92e8
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-05 05:02:33 +0100

    Use Ed25519 for Validation

--- plugins/validation.js
+++ plugins/validation.js
@@ -11,7 +11,7 @@ const fp = require("fastify-plugin");
  */
 module.exports = fp(async function (fastify, opts) {
   fastify.decorate("validateWebAppData", async function (request, reply) {
-    if (!fastify.utils.isValidInitData(request.body.initData)) {
+    if (!fastify.utils.isValidEd25519InitData(request.body.initData)) {
       return reply.forbidden("Invalid InitData!");
     } else {
       request.initDataUnsafe = fastify.utils.getInitDataUnsafe(
