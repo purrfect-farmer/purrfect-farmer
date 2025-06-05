4d9480a89deae1742801a1c69294198bbffa87d2
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-05 05:01:59 +0100

    Added initData plugins

---
+++ plugins/validation.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+
+// the use of fastify-plugin is required to be able
+// to export the decorators to the outer scope
+
+/**
+ * @param {import("fastify").FastifyInstance} fastify
+ * @param {object} opts
+ */
+module.exports = fp(async function (fastify, opts) {
+  fastify.decorate("validateWebAppData", async function (request, reply) {
+    if (!fastify.utils.isValidInitData(request.body.initData)) {
+      return reply.forbidden("Invalid InitData!");
+    } else {
+      request.initDataUnsafe = fastify.utils.getInitDataUnsafe(
+        request.body.initData
+      );
+    }
+  });
+});
