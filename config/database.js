a98ed1e5fc05756a620380c9bf7cfe53e1464f5c
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-08 17:00:15 +0100

    Refactored database config

--- config/database.js
+++ config/database.js
@@ -10,6 +10,6 @@ module.exports = {
   production: {
     dialect: "sqlite",
     storage: "./db/database.sqlite",
-    ...(process.env.NODE_ENV === "production" && { logging: false }),
+    logging: false,
   },
 };
