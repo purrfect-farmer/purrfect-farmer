f2ab3ee6e57e1e8200f915b59192b547f6821eff
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-15 17:42:59 +0100

    Removed Sandbox

--- src/app/Browser.jsx
+++ src/app/Browser.jsx
@@ -14,7 +14,7 @@ export default memo(function Browser({ url }) {
       src={url}
       className="w-full h-full border-0 outline-0"
       referrerPolicy="no-referrer"
-      allow="accelerometer; autoplay; camera; clipboard-write; encrypted-media; fullscreen; gamepad; geolocation; gyroscope; magnetometer; microphone; midi; payment; picture-in-picture; usb; xr-spatial-tracking; screen-wake-lock; web-share; idle-detection"
+      allow="accelerometer; autoplay; camera; clipboard-write; encrypted-media; fullscreen; gamepad; geolocation; gyroscope; magnetometer; microphone; midi; payment; picture-in-picture; usb; xr-spatial-tracking; screen-wake-lock; idle-detection"
     />
   );
 });
