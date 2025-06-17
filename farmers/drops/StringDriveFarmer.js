e2e74b0c530d51c0e4cf35689c9437f1b1bd5788
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-17 22:06:33 +0100

    Play Game

--- farmers/drops/StringDriveFarmer.js
+++ farmers/drops/StringDriveFarmer.js
@@ -38,6 +38,7 @@ module.exports = class StringDriveFarmer extends BaseFarmer {
         )
     );
 
+    /** Ads */
     const userAds = await this.api
       .get(this.path("https://st-ba-drive.stringdrive.io/api/auth/getUserads"))
       .then((res) => res.data.data);
@@ -51,6 +52,46 @@ module.exports = class StringDriveFarmer extends BaseFarmer {
       await this.tryToJoinTelegramLink(task["Sitelink"]);
       await this.completeUserTask(task["_id"]);
       await utils.delayForSeconds(20);
+    }
+
+    /** Game */
+    await this.playGame();
+  }
+
+  async playGame() {
+    const gameId = "682c52efdc400a61db731520";
+    const game = await this.api
+      .get(
+        this.path(
+          "https://st-ba-drive.stringdrive.io/api/auth/getusersinglegame",
+          gameId
+        )
+      )
+      .then((res) => res.data.data);
+
+    const max = Number(game.max);
+    const user = await this.getProfile();
+    const balance = Number(user.ticketBalance);
+
+    if (balance >= max) {
+      const bet = await this.api
+        .post(this.path("https://st-ba-drive.stringdrive.io/api/auth/game"), {
+          betAmount: String(max),
+          gameId,
+        })
+        .then((res) => res.data.data);
+
+      const gameHistoryId = bet.gameHistoryId;
+
+      await utils.delayForSeconds(60);
+
+      await this.api
+        .post(this.path("https://st-ba-drive.stringdrive.io/api/auth/game"), {
+          gameHistoryId,
+          playedStatus: utils.randomItem(["WON", "LOSE"]),
+          winAmount: utils.extraGamePoints(max),
+        })
+        .then((res) => res.data.data);
     }
   }
 
@@ -105,10 +146,10 @@ module.exports = class StringDriveFarmer extends BaseFarmer {
   async getProfile() {
     return await this.api
       .get(this.path("https://st-ba-drive.stringdrive.io/api/auth/getprofile"))
-      .then((res) => res.data);
+      .then((res) => res.data.user);
   }
 
-  path(path) {
-    return `${path}/${this.auth.user.id}`;
+  path(path, id) {
+    return `${path}/${id || this.auth.user.id}`;
   }
 };
