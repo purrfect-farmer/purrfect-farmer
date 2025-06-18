24a6c13338be35aef098f810d03de5decf2f3899
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-18 06:33:59 +0100

    Reordered setting groups

--- src/partials/Settings.jsx
+++ src/partials/Settings.jsx
@@ -464,84 +464,6 @@ export default memo(function Settings({ tabs }) {
                         </LabelToggle>
                       </SettingsGroup>
 
-                      {/* Proxy Options */}
-                      <SettingsGroup id={"proxy"} title={"Proxy Options"}>
-                        {/* Enable Proxy */}
-                        <LabelToggle
-                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
-                          onChange={(ev) =>
-                            configureSharedSettings(
-                              "proxyEnabled",
-                              ev.target.checked
-                            )
-                          }
-                          checked={sharedSettings.proxyEnabled}
-                        >
-                          Enable Proxy
-                        </LabelToggle>
-
-                        <p className="text-neutral-500 dark:text-neutral-400">
-                          To enable this option, first turn off proxy sharing in
-                          Cloud options.
-                        </p>
-
-                        {/* Proxy Host */}
-                        <SettingsLabel>Proxy Host</SettingsLabel>
-                        <Input
-                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
-                          value={sharedSettings.proxyHost}
-                          onChange={(ev) =>
-                            configureSharedSettings(
-                              "proxyHost",
-                              ev.target.value
-                            )
-                          }
-                          placeholder="Proxy Host"
-                        />
-
-                        {/* Proxy Port */}
-                        <SettingsLabel>Proxy Port</SettingsLabel>
-                        <Input
-                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
-                          value={sharedSettings.proxyPort}
-                          onChange={(ev) =>
-                            configureSharedSettings(
-                              "proxyPort",
-                              ev.target.value
-                            )
-                          }
-                          placeholder="Proxy Port"
-                        />
-
-                        {/* Proxy Username */}
-                        <SettingsLabel>Proxy Username</SettingsLabel>
-                        <Input
-                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
-                          value={sharedSettings.proxyUsername}
-                          onChange={(ev) =>
-                            configureSharedSettings(
-                              "proxyUsername",
-                              ev.target.value
-                            )
-                          }
-                          placeholder="Proxy Username"
-                        />
-
-                        {/* Proxy Password */}
-                        <SettingsLabel>Proxy Password</SettingsLabel>
-                        <Input
-                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
-                          value={sharedSettings.proxyPassword}
-                          onChange={(ev) =>
-                            configureSharedSettings(
-                              "proxyPassword",
-                              ev.target.value
-                            )
-                          }
-                          placeholder="Proxy Password"
-                        />
-                      </SettingsGroup>
-
                       {/* Bot Options */}
                       <SettingsGroup id={"bot"} title={"Bot Options"}>
                         {/* Enable In-App Browser */}
@@ -712,6 +634,84 @@ export default memo(function Settings({ tabs }) {
                           {/* Set Button */}
                           <ConfirmButton onClick={handleSetSeekerServer} />
                         </div>
+                      </SettingsGroup>
+
+                      {/* Proxy Options */}
+                      <SettingsGroup id={"proxy"} title={"Proxy Options"}>
+                        {/* Enable Proxy */}
+                        <LabelToggle
+                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
+                          onChange={(ev) =>
+                            configureSharedSettings(
+                              "proxyEnabled",
+                              ev.target.checked
+                            )
+                          }
+                          checked={sharedSettings.proxyEnabled}
+                        >
+                          Enable Proxy
+                        </LabelToggle>
+
+                        <p className="text-neutral-500 dark:text-neutral-400">
+                          To enable this option, first turn off proxy sharing in
+                          Cloud options.
+                        </p>
+
+                        {/* Proxy Host */}
+                        <SettingsLabel>Proxy Host</SettingsLabel>
+                        <Input
+                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
+                          value={sharedSettings.proxyHost}
+                          onChange={(ev) =>
+                            configureSharedSettings(
+                              "proxyHost",
+                              ev.target.value
+                            )
+                          }
+                          placeholder="Proxy Host"
+                        />
+
+                        {/* Proxy Port */}
+                        <SettingsLabel>Proxy Port</SettingsLabel>
+                        <Input
+                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
+                          value={sharedSettings.proxyPort}
+                          onChange={(ev) =>
+                            configureSharedSettings(
+                              "proxyPort",
+                              ev.target.value
+                            )
+                          }
+                          placeholder="Proxy Port"
+                        />
+
+                        {/* Proxy Username */}
+                        <SettingsLabel>Proxy Username</SettingsLabel>
+                        <Input
+                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
+                          value={sharedSettings.proxyUsername}
+                          onChange={(ev) =>
+                            configureSharedSettings(
+                              "proxyUsername",
+                              ev.target.value
+                            )
+                          }
+                          placeholder="Proxy Username"
+                        />
+
+                        {/* Proxy Password */}
+                        <SettingsLabel>Proxy Password</SettingsLabel>
+                        <Input
+                          disabled={Boolean(import.meta.env.VITE_WHISKER)}
+                          value={sharedSettings.proxyPassword}
+                          onChange={(ev) =>
+                            configureSharedSettings(
+                              "proxyPassword",
+                              ev.target.value
+                            )
+                          }
+                          placeholder="Proxy Password"
+                        />
                       </SettingsGroup>
 
                       {!import.meta.env.VITE_WHISKER ? (
