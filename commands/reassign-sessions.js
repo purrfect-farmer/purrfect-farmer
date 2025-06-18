c366ac5ad960aced35bbaf12027fa2d2866f75a0
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-18 09:48:06 +0100

    Reassign sessions

---
+++ commands/reassign-sessions.js
@@ -0,0 +1,38 @@
+/**
+ * @param {import("commander").Command} program
+ * @param {typeof import("inquirer").default} inquirer
+ * @param {typeof import("chalk").default} chalk
+ */
+module.exports = (program, inquirer, chalk) => {
+  program
+    .command("reassign-sessions")
+    .description("Reassign Telegram Sessions")
+    .action(async () => {
+      const GramClient = require("../lib/GramClient");
+      const sessions = await GramClient.getSessions();
+      const db = require("../db/models");
+
+      let count = 0;
+
+      for (const session of sessions) {
+        const client = await GramClient.create(session);
+        const user = await client.getSelf();
+
+        if (user) {
+          await db.Account.update(
+            { session },
+            {
+              where: {
+                id: user.id,
+              },
+            }
+          );
+          count++;
+        } else {
+          await client.logout();
+        }
+      }
+
+      console.log(chalk.green.bold(`Reassigned sessions: ${count}`));
+    });
+};
