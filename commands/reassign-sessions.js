ed39f5e82b6bc49f34c867506d8cc9a290f49b20
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-18 10:19:46 +0100

    Refactor

--- commands/reassign-sessions.js
+++ commands/reassign-sessions.js
@@ -9,31 +9,30 @@ module.exports = (program, inquirer, chalk) => {
     .description("Reassign Telegram Sessions")
     .action(async () => {
       const GramClient = require("../lib/GramClient");
-      const sessions = await GramClient.getSessions();
       const db = require("../db/models");
+
+      const sessions = await GramClient.getSessions();
       const assigned = [];
 
       for (const session of sessions) {
         const client = await GramClient.create(session);
         const user = await client.getSelf();
 
-        if (user) {
-          if (assigned.includes(user.id)) {
-            await client.logout();
-          } else {
-            const account = await db.Account.findByPk(user.id);
-
-            if (!account) {
-              await client.logout();
-            } else {
-              await client.destroy();
-              await account.update({ session });
-              await assigned.push(user.id);
-            }
-          }
-        } else {
+        if (!user || assigned.includes(user.id)) {
           await client.logout();
+          continue;
         }
+
+        const account = await db.Account.findByPk(user.id);
+
+        if (!account) {
+          await client.logout();
+          continue;
+        }
+
+        await client.destroy();
+        await account.update({ session });
+        assigned.push(user.id);
       }
 
       console.log(chalk.green.bold(`Reassigned sessions: ${assigned.length}`));
