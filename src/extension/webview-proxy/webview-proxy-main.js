aa7cebcb79aeeb3d7b9765e18641a133ec660f3d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-19 16:24:36 +0100

    Ensure source is instance of EventTarget

--- src/extension/webview-proxy/webview-proxy-main.js
+++ src/extension/webview-proxy/webview-proxy-main.js
@@ -1,32 +1,70 @@
 import { customLogger, withValue } from "@/lib/utils";
 
 export function injectTelegramWebviewProxy() {
-  /** Cross Origin */
-  if (window.self !== window.top) {
-    /** Custom Message Event */
-    class CustomMessageEvent extends MessageEvent {
-      constructor(type, options) {
-        /** Log */
-        customLogger("MESSAGE EVENT", type, options);
+  const NativeMessageEvent = MessageEvent;
 
-        const safeOptions = { ...options };
+  /** Custom Message Event */
+  class CustomMessageEvent extends NativeMessageEvent {
+    constructor(type, options) {
+      const safeOptions = { ...options };
 
-        if (options?.source && !(options.source instanceof Window)) {
-          safeOptions.source = window;
-        }
-
-        super(type, safeOptions);
-        this._originalOptions = options;
+      if (options?.source && !(options.source instanceof EventTarget)) {
+        safeOptions.source = window;
       }
 
-      get source() {
-        return this._originalOptions?.source || window;
-      }
+      super(type, safeOptions);
+      this._originalOptions = options;
     }
 
-    /** Custom Message Event */
-    window.MessageEvent = CustomMessageEvent;
+    get source() {
+      return this._originalOptions?.source || window;
+    }
+  }
 
+  /** Custom Message Event */
+  window.MessageEvent = CustomMessageEvent;
+
+  /** Capture Message */
+  window.addEventListener("message", (ev) => {
+    if (ev instanceof CustomMessageEvent || typeof ev.data !== "string") return;
+
+    let event;
+    try {
+      event = JSON.parse(ev.data);
+    } catch (e) {
+      return;
+    }
+
+    const { eventType, eventData } = event || {};
+    if (!eventType) return;
+
+    /** Stop Immediate Propagation */
+    ev.stopImmediatePropagation();
+
+    /** Receive Event */
+    window.Telegram?.WebView?.receiveEvent?.(eventType, eventData);
+  });
+
+  /** Mimic Telegram Webview Proxy */
+  window.TelegramWebviewProxy = {
+    postEvent(type, data) {
+      const eventType = type;
+      const eventData = typeof data !== "undefined" ? JSON.parse(data) : data;
+
+      customLogger("TELEGRAM WEBVIEW PROXY", eventType, eventData);
+
+      return window.parent.postMessage(
+        JSON.stringify({
+          eventType,
+          eventData,
+        }),
+        "*"
+      );
+    },
+  };
+
+  /** Cross Origin */
+  if (window.self !== window.top) {
     /** Override Window Parent */
     window.parent = withValue(window.parent, (parent) => {
       return {
@@ -45,45 +83,5 @@ export function injectTelegramWebviewProxy() {
         },
       };
     });
-
-    /** Capture Message */
-    window.addEventListener("message", (ev) => {
-      if (ev instanceof CustomMessageEvent || typeof ev.data !== "string")
-        return;
-
-      let event;
-      try {
-        event = JSON.parse(ev.data);
-      } catch (e) {
-        return;
-      }
-
-      const { eventType, eventData } = event || {};
-      if (!eventType) return;
-
-      /** Stop Immediate Propagation */
-      ev.stopImmediatePropagation();
-
-      /** Receive Event */
-      window.Telegram?.WebView?.receiveEvent?.(eventType, eventData);
-    });
-
-    /** Mimic Telegram Webview Proxy */
-    window.TelegramWebviewProxy = {
-      postEvent(type, data) {
-        const eventType = type;
-        const eventData = typeof data !== "undefined" ? JSON.parse(data) : data;
-
-        customLogger("TELEGRAM WEBVIEW PROXY", eventType, eventData);
-
-        return window.parent.postMessage(
-          JSON.stringify({
-            eventType,
-            eventData,
-          }),
-          "*"
-        );
-      },
-    };
   }
 }
