04f855dce0a35f9705c2294797c97969e13130d2
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-11 14:50:27 +0100

    feat: Added env helper

--- apps/purrfect-fly/config/env.js
+++ apps/purrfect-fly/config/env.js
@@ -8,3 +8,32 @@ const __dirname = path.dirname(__filename);
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
+
+global.env = function (key, defaultValue = null) {
+  let value = process.env[key];
+
+  if (value === undefined) return defaultValue;
+
+  value = value.trim();
+
+  switch (value.toLowerCase()) {
+    case "true":
+    case "(true)":
+      return true;
+    case "false":
+    case "(false)":
+      return false;
+    case "null":
+    case "(null)":
+      return null;
+    case "empty":
+    case "(empty)":
+      return "";
+  }
+
+  if (!isNaN(value) && value !== "") {
+    return Number(value);
+  }
+
+  return value;
+};
