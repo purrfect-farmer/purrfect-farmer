5e987688dc8bb61f4b2736d09473119cac7337de
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-12 10:14:55 +0100

    feat: SWAP USDT, Complete Tasks and upgrade levels

--- packages/shared/farmers/iMinerFarmer.js
+++ packages/shared/farmers/iMinerFarmer.js
@@ -93,11 +93,70 @@ export default class iMinerFarmer extends BaseFarmer {
       .then((res) => res.data.data);
   }
 
+  /** Get Event Experience */
+  getEventExperience(signal = this.signal) {
+    return this.api
+      .get(
+        "https://apimain.iminer.fun/iapi/server/event/invest/experience/running",
+        { signal }
+      )
+      .then((res) => res.data.data);
+  }
+
+  /** Get Event Experience Details */
+  getEventExperienceDetails(id, signal = this.signal) {
+    return this.api
+      .get(
+        `https://apimain.iminer.fun/iapi/server/event/invest/experience/detail?id=${id}`,
+        { signal }
+      )
+      .then((res) => res.data.data);
+  }
+
+  /** Claim Event Experience */
+  claimEventExperience(id, signal = this.signal) {
+    return this.api
+      .post(
+        `https://apimain.iminer.fun/iapi/server/event/invest/experience/claim`,
+        { id },
+        { signal }
+      )
+      .then((res) => res.data.data);
+  }
+
   /** Get Levels */
   getLevels(signal = this.signal) {
     return this.api
       .get(
         "https://apimain.iminer.fun/iapi/server/speed/page?page=1&limit=50&type=2",
+        { signal }
+      )
+      .then((res) => res.data.data.list);
+  }
+
+  /** Get Wallets */
+  getWallets(signal = this.signal) {
+    return this.api
+      .get("https://apimain.iminer.fun/iapi/server/wallet/list", { signal })
+      .then((res) => res.data.data);
+  }
+
+  /** Swap Currency */
+  swapCurrency(fromCurrencyId, toCurrencyId, amount, signal = this.signal) {
+    return this.api
+      .post(
+        "https://apimain.iminer.fun/iapi/server/wallet/swap",
+        { fromCurrencyId, toCurrencyId, amount },
+        { signal }
+      )
+      .then((res) => res.data.data);
+  }
+
+  /** Upgrade Level */
+  createOrder(configId, signal = this.signal) {
+    return this.api
+      .get(
+        `https://apimain.iminer.fun/iapi/server/speed/create/order?configId=${configId}`,
         { signal }
       )
       .then((res) => res.data.data);
@@ -110,6 +169,9 @@ export default class iMinerFarmer extends BaseFarmer {
     this.logUserInfo(user);
 
     await this.executeTask("Mining", () => this.completeMining(user));
+    await this.executeTask("Tasks", () => this.completeTasks(user));
+    await this.executeTask("SWAP USDT", () => this.swapUSDT(user));
+    await this.executeTask("Levels", () => this.upgradeLevels(user));
   }
 
   logUserInfo(user) {
@@ -127,21 +189,64 @@ export default class iMinerFarmer extends BaseFarmer {
   async completeTasks(user) {
     const tasks = await this.getTasks();
     for (const task of tasks) {
-      if (task.title.includes("miner")) continue;
-
       /** Join Telegram Link */
       await this.tryToJoinTelegramLink(task.linkUrl);
 
       if (task.doneTask === -1) {
         await this.confirmTask(task.id);
-        this.logger.success(
-          `✅ Task "${task.title.toUpperCase()}" confirmed successfully!`
-        );
+        this.logger.success(`✅ ${task.taskName.toUpperCase()} - confirmed`);
       } else if (task.doneTask === 1 && task.claimStatus !== 1) {
         await this.claimTaskReward(task.id);
         this.logger.success(
-          `✅ Task "${task.title.toUpperCase()}" reward claimed successfully!`
+          `🎊 ${task.taskName.toUpperCase()} - reward claimed`
         );
+      }
+    }
+  }
+
+  /** Swap USDT to IMR */
+  async swapUSDT() {
+    const wallets = await this.getWallets();
+    const imrWallet = wallets.find((w) => w.currencyVO.currency === "IMR");
+    const usdtWallet = wallets.find((w) => w.currencyVO.currency === "USDT");
+
+    if (usdtWallet.balance > 0) {
+      await this.swapCurrency(
+        usdtWallet.currencyId,
+        imrWallet.currencyId,
+        Number(usdtWallet.balance)
+      );
+      this.logger.success(
+        `💱 Swapped ${usdtWallet.balance} USDT to IMR successfully!`
+      );
+    }
+  }
+
+  async upgradeLevels() {
+    const user = await this.getUser();
+    const wallets = await this.getWallets();
+    const imrWallet = wallets.find((w) => w.currencyVO.currency === "IMR");
+    const levels = await this.getLevels();
+
+    let balance = Number(imrWallet.balance);
+    let currentLevel = user.miningLevel;
+
+    while (true) {
+      const nextLevel = levels.find(
+        (level) => level.level === currentLevel + 1
+      );
+      if (!nextLevel) break; // No more levels to upgrade
+
+      if (balance >= nextLevel.cost) {
+        await this.createOrder(nextLevel.id);
+        balance -= nextLevel.cost;
+        currentLevel = nextLevel.level;
+        this.logger.success(
+          `🚀 Upgraded to level ${nextLevel.level} successfully!`
+        );
+        await this.utils.delayForSeconds(2);
+      } else {
+        break; // Not enough balance to upgrade further
       }
     }
   }
