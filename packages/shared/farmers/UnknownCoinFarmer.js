5466f73748ae908947de05e7077f104e07a48f68
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-16 05:12:38 +0100

    feat: Complete Shake, Pop It and Ads Rewards

--- packages/shared/farmers/UnknownCoinFarmer.js
+++ packages/shared/farmers/UnknownCoinFarmer.js
@@ -37,9 +37,14 @@ export default class UnknownCoinFarmer extends BaseFarmer {
 
   /** Get signature headers */
   async getSignatureHeaders(url) {
-    const path = new URL(url).pathname.replace("/api/", "");
-    const data = `${path}:${timestamp}`;
+    /** Get Current Timestamp */
     const timestamp = Math.floor(Date.now() / 1000);
+
+    /** Get Request Path */
+    const path = new URL(url).pathname.replace("/api/", "");
+
+    /** Create Signature */
+    const data = `${path}:${timestamp}`;
     const signature = this.utils.sha256Hmac(this.constructor.SECRET_KEY, data);
 
     return {
@@ -58,9 +63,7 @@ export default class UnknownCoinFarmer extends BaseFarmer {
   /** Get Auth */
   async fetchAuth() {
     /** Get Server Time */
-    const { time } = await this.getServerTime();
-
-    this._timestamp = time;
+    await this.getServerTime();
     this._userData = await this.getUserInfo();
 
     return this._userData;
@@ -140,15 +143,99 @@ export default class UnknownCoinFarmer extends BaseFarmer {
       .then((res) => res.data);
   }
 
+  /** Get Tasks */
+  getTasks(signal = this.signal) {
+    return this.api
+      .get("https://api.unknown-coin.com/api/tasks", { signal })
+      .then((res) => res.data.data);
+  }
+
+  /** Add Energy From Ads */
+  addEnergyFromAds(type, signal = this.signal) {
+    return this.api
+      .post(
+        "https://api.unknown-coin.com/api/challenges/add-energy-from-ads",
+        { type },
+        {
+          signal,
+        }
+      )
+      .then((res) => res.data);
+  }
+
+  /** Add Energy */
+  addEnergy(amount, signal = this.signal) {
+    return this.api
+      .post(
+        "https://api.unknown-coin.com/api/add-energy",
+        { amount },
+        { signal }
+      )
+      .then((res) => res.data);
+  }
+
+  /** Add Energy From Shake */
+  addEnergyFromShake(amount = 500, signal = this.signal) {
+    return this.api
+      .post(
+        "https://api.unknown-coin.com/api/challenges/shake/add-energy",
+        { amount },
+        { signal }
+      )
+      .then((res) => res.data);
+  }
+
   /** Process Farmer */
   async process() {
-    const { user } = this._userData;
+    const user = this._userData;
 
     this.logUserInfo(user);
+    await this.executeTask("Ads", () => this.completeAdRewards(user));
+    await this.executeTask("Pop It", () => this.completePopIt(user));
+    await this.executeTask("Shake", () => this.completeShake(user));
+    await this.executeTask("Tasks", () => this.completeTasks(user));
   }
 
   logUserInfo(user) {
     this.logger.newline();
     this.logCurrentUser();
+    this.logger.keyValue("Balance", user.balance);
+    this.logger.keyValue("Energy Rounds", user["energy_rounds"]);
+    this.logger.keyValue("USDT", user["usdt_balance"]);
+    this.logger.keyValue("TON", user["ton_balance"]);
+    this.logger.keyValue("NOT", user["not_balance"]);
+  }
+
+  async completeAdRewards(user) {
+    for (const [category, ads] of Object.entries(user["ads_rewards"])) {
+      if (ads["count"] > ads["used"]) {
+        await this.addEnergyFromAds(category);
+        this.logger.success(`+Energy from ${category.toUpperCase()} Ads`);
+        await this.utils.delayForSeconds(5);
+      }
+    }
+  }
+
+  async completePopIt(user) {
+    for (let i = 0; i < user["energy_rounds"]; i++) {
+      const energy = 150 + Math.floor(Math.random() * 50);
+      await this.addEnergy(energy);
+      this.logger.success(`+${energy} Energy from Pop It`);
+      await this.utils.delayForSeconds(5);
+    }
+  }
+
+  async completeShake(user) {
+    const { time } = await this.getServerTime();
+    const nextShake = user["challenges"]["next_shake_round_at"];
+
+    if (time >= nextShake) {
+      await this.addEnergyFromShake();
+      this.logger.success(`+500 Energy from Shake`);
+    }
+  }
+
+  async completeTasks(user) {
+    const tasks = await this.getTasks();
   }
 }
