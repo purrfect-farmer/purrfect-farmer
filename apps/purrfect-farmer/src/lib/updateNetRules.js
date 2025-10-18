e4af6007939148128f9ab2462f06bf78a4e3f157
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-18 09:33:38 +0100

    feat: Apply net rules to Whiskers

--- apps/purrfect-farmer/src/lib/updateNetRules.js
+++ apps/purrfect-farmer/src/lib/updateNetRules.js
@@ -3,18 +3,25 @@ import { getNetRules } from "@/extension/rule-resources";
 import { customLogger, getUserAgent, storeUserAgent } from "./utils";
 
 export default async function updateNetRules() {
-  if (typeof chrome?.declarativeNetRequest === "undefined") {
-    return;
-  }
-
   const userAgent = await getUserAgent();
-
-  const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
-  const oldRuleIds = oldRules.map((rule) => rule.id);
   const newRules = getNetRules(userAgent).map((item, index) => ({
     ...item,
     id: index + 1,
   }));
+
+  if (import.meta.env.VITE_WHISKER) {
+    await window.electron.ipcRenderer.invoke(
+      "update-declarative-net-rules",
+      newRules
+    );
+  }
+
+  if (typeof chrome?.declarativeNetRequest === "undefined") {
+    return;
+  }
+
+  const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+  const oldRuleIds = oldRules.map((rule) => rule.id);
 
   /** Log */
   customLogger("DECLARATIVE NET RULES", newRules);
