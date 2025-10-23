f4724deca32ece1758ebbf5c49f3870c3556568a
Sadiq Salau <sadiqsalau888@gmail.com>
2025-10-23 06:16:34 +0100

    feat: Moved Game to last task

--- packages/shared/farmers/MoneyTreeFarmer.js
+++ packages/shared/farmers/MoneyTreeFarmer.js
@@ -270,9 +270,9 @@ export default class MoneyTreeFarmer extends BaseFarmer {
     await this.executeTask("Daily Bonus", () => this.claimDailyBonus());
     await this.executeTask("Claim Tickets", () => this.claimTickets());
     await this.executeTask("Claim Free Boosts", () => this.claimFreeBoosts());
-    await this.executeTask("Play Game", () => this.playGame());
     await this.executeTask("Upgrade Boosts", () => this.upgradeBoosts());
     await this.executeTask("Auto Bot", () => this.claimOrPurchaseAutoBot());
+    await this.executeTask("Play Game", () => this.playGame());
   }
   /** Log User Info */
   logUserInfo(user) {
