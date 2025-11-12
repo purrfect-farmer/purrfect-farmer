571d26f2ed0a49e68ad40c3aade65bb029e9a422
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-12 07:36:07 +0100

    feat: Removed minimum height

--- apps/purrfect-farmer/src/components/Input.jsx
+++ apps/purrfect-farmer/src/components/Input.jsx
@@ -7,7 +7,7 @@ export default memo(function Input(props) {
       {...props}
       className={cn(
         "bg-neutral-100 dark:bg-neutral-700",
-        "p-2.5 rounded-lg font-bold w-full min-h-0 min-w-0",
+        "p-2.5 rounded-lg font-bold w-full min-w-0",
         "focus:outline-hidden focus:ring-3 focus:ring-blue-300",
         "disabled:opacity-50",
         props.className
