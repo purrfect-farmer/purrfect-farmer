d4e8686aa3f0fd8bcbca5c6a4c1e7fa6d44bfeb0
Sadiq Salau <sadiqsalau888@gmail.com>
2025-11-23 13:52:45 +0100

    feat: Added support for CaptchaSonic / NoCaptchaAi

--- packages/shared/lib/CaptchaSolver.js
+++ packages/shared/lib/CaptchaSolver.js
@@ -4,22 +4,46 @@ export default class CaptchaSolver {
   constructor(provider, apiKey) {
     this.provider = provider;
     this.apiKey = apiKey;
+
     let baseURL = "";
+    let taskBased = false;
+    let taskType = "";
+    let authProperty = "";
 
     switch (this.provider) {
       case "2captcha":
         baseURL = "https://2captcha.com";
         break;
+
       case "captchaai":
         baseURL = "https://ocr.captchaai.com";
         break;
+
       case "solvecaptcha":
         baseURL = "https://api.solvecaptcha.com";
         break;
+
+      case "captchasonic":
+        taskBased = true;
+        authProperty = "apiKey";
+        taskType = "AntiTurnstileTaskProxyLess";
+        baseURL = "https://api.captchasonic.com";
+        break;
+
+      case "nocaptchaai":
+        taskBased = true;
+        authProperty = "clientKey";
+        taskType = "AntiTurnstileTask";
+        baseURL = "https://api.nocaptchaai.com";
+        break;
+
       default:
         throw new Error(`Unsupported captcha provider: ${this.provider}`);
     }
 
+    this.taskBased = taskBased;
+    this.authProperty = authProperty;
+    this.taskType = taskType;
     this.api = axios.create({
       baseURL: baseURL,
       timeout: 120_000,
@@ -28,34 +52,64 @@ export default class CaptchaSolver {
 
   /** Check if configured */
   isConfigured() {
-    return Boolean(this.apiKey);
+    return Boolean(this.provider && this.apiKey);
   }
 
   /** Get Turnstile Request */
   getTurnstileRequest({ siteKey, pageUrl }) {
-    return this.api
-      .post("/in.php", {
-        key: this.apiKey,
-        method: "turnstile",
-        sitekey: siteKey,
-        pageurl: pageUrl,
-        json: 1,
-      })
-      .then((res) => res.data);
+    if (this.taskBased) {
+      return this.api
+        .post("/createTask", {
+          [this.authProperty]: this.apiKey,
+          task: {
+            type: this.taskType,
+            websiteURL: pageUrl,
+            websiteKey: siteKey,
+          },
+        })
+        .then((res) => ({
+          ...res.data,
+          request: res.data.taskId,
+        }));
+    } else {
+      return this.api
+        .post("/in.php", {
+          key: this.apiKey,
+          method: "turnstile",
+          sitekey: siteKey,
+          pageurl: pageUrl,
+          json: 1,
+        })
+        .then((res) => res.data);
+    }
   }
 
   /** Get Turnstile Result */
   getTurnstileResult(requestId) {
-    return this.api
-      .get("/res.php", {
-        params: {
-          key: this.apiKey,
-          action: "get",
-          id: requestId,
-          json: 1,
-        },
-      })
-      .then((res) => res.data);
+    if (this.taskBased) {
+      return this.api
+        .post("/getTaskResult", {
+          [this.authProperty]: this.apiKey,
+          taskId: requestId,
+        })
+        .then((res) => {
+          return {
+            status: res.data.errorId === 0 ? 1 : 0,
+            request: res.data.solution ? res.data.solution.token : null,
+          };
+        });
+    } else {
+      return this.api
+        .get("/res.php", {
+          params: {
+            key: this.apiKey,
+            action: "get",
+            id: requestId,
+            json: 1,
+          },
+        })
+        .then((res) => res.data);
+    }
   }
 
   /** Solve Turnstile */
