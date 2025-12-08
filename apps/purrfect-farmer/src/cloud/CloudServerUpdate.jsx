4da59da23b250baad4cbd014fde8498e4a728264
Sadiq Salau <sadiqsalau888@gmail.com>
2025-12-08 19:06:17 +0100

    feat: Display update details

--- apps/purrfect-farmer/src/cloud/CloudServerUpdate.jsx
+++ apps/purrfect-farmer/src/cloud/CloudServerUpdate.jsx
@@ -4,6 +4,43 @@ import toast from "react-hot-toast";
 import CloudCenteredDialog from "./CloudCenteredDialog";
 import useCloudManagerServerUpdateMutation from "@/hooks/useCloudManagerServerUpdateMutation";
 import Alert from "@/components/Alert";
+import AnsiToHtml from "ansi-to-html";
+import { cn } from "@/lib/utils";
+
+const ansiToHtml = new AnsiToHtml();
+
+const ServerUpdateDetails = ({ data }) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <h3 className="font-semibold text-center uppercase text-neutral-500">
+        Update Details
+      </h3>
+      <pre
+        className={cn(
+          "overflow-auto bg-black text-white p-2 max-h-96",
+          "font-mono whitespace-pre-wrap wrap-break-word"
+        )}
+        dangerouslySetInnerHTML={{
+          __html: ansiToHtml.toHtml(data.stdout) || "No output available.",
+        }}
+      />
+      {data.stderr && (
+        <>
+          <h4 className="font-semibold text-center uppercase text-neutral-500">
+            Errors
+          </h4>
+          <pre
+            className={cn(
+              "overflow-auto bg-black text-white p-2 max-h-96",
+              "font-mono whitespace-pre-wrap wrap-break-word"
+            )}
+            dangerouslySetInnerHTML={{ __html: ansiToHtml.toHtml(data.stderr) }}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default function CloudServerUpdate() {
   const serverUpdateMutation = useCloudManagerServerUpdateMutation();
@@ -29,6 +66,10 @@ export default function CloudServerUpdate() {
           Updating your server may cause temporary downtime. Please be patient
           while the update is in progress.
         </Alert>
+
+        {serverUpdateMutation.data && (
+          <ServerUpdateDetails data={serverUpdateMutation.data} />
+        )}
 
         {/* Update Button */}
         <PrimaryButton disabled={isPending} onClick={updateServer}>
