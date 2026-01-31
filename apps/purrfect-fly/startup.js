4f631431d3bd298d8a5b250673ad48b73894610f
Sadiq Salau <sadiqsalau888@gmail.com>
2026-01-31 20:58:51 +0100

    refactor: Refactored startup script

--- apps/purrfect-fly/startup.js
+++ apps/purrfect-fly/startup.js
@@ -1,19 +1,20 @@
-import axios from "axios";
-
+import GramClient from "./lib/GramClient.js";
 import app from "./config/app.js";
+import axios from "axios";
 import bot from "./lib/bot.js";
 import cleanDatabase from "./actions/clean-database.js";
-import GramClient from "./lib/GramClient.js";
 
 if (app.seeker.enabled || app.startup.sendServerAddress) {
-  axios
-    .get("http://checkip.amazonaws.com", { timeout: 5000 })
-    .then((response) => {
-      const address = `http://${String(response.data).trim()}`;
+  /** Fetch and send server address */
+  const fetchAndSendServerAddress = async () => {
+    try {
+      const address = await axios
+        .get("http://checkip.amazonaws.com", { timeout: 5000 })
+        .then((response) => `http://${String(response.data).trim()}`);
 
       /** Post to Seeker Server */
       if (app.seeker.enabled) {
-        axios
+        await axios
           .post(
             `${app.seeker.server}/api/servers/update`,
             {
@@ -21,7 +22,7 @@ if (app.seeker.enabled || app.startup.sendServerAddress) {
               name: app.name,
               address,
             },
-            { timeout: 5000 }
+            { timeout: 5000 },
           )
           .catch((error) => {
             console.error("Failed to Update Seeker:", error);
@@ -36,18 +37,27 @@ if (app.seeker.enabled || app.startup.sendServerAddress) {
           console.error("Failed Send IP Address Notification:", error);
         }
       }
-    })
-    .catch((error) => console.error("Failed to get IP Address:", error));
+    } catch (e) {
+      console.error("Error while retrieving and sending server address:", e);
+    }
+  };
+
+  /** Fetch and send server address */
+  fetchAndSendServerAddress();
 }
 
 /** Remove old farmers */
-await cleanDatabase();
+try {
+  await cleanDatabase();
+} catch (e) {
+  console.error("Error while cleaning database:", e);
+}
 
 /** Handle Graceful Shutdown */
 process.on("SIGINT", async () => {
   console.log("Gracefully shutting down...");
   await Promise.allSettled(
-    [...GramClient.instances.values()].map((c) => c.destroy())
+    [...GramClient.instances.values()].map((c) => c.destroy()),
   );
   process.exit(0);
 });
