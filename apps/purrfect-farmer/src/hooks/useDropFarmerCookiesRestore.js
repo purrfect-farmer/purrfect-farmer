78bdbc5cec7adb0589ba442ca06173a72a2f6960
Sadiq Salau <sadiqsalau888@gmail.com>
2026-02-05 15:25:45 +0100

    fix: Cookies restore

--- apps/purrfect-farmer/src/hooks/useDropFarmerCookiesRestore.js
+++ apps/purrfect-farmer/src/hooks/useDropFarmerCookiesRestore.js
@@ -1,10 +1,27 @@
-import { setCookies } from "@/utils/index";
+import { setCookies } from "@/utils";
 import { useEffect } from "react";
 
 export default function useDropFarmerCookiesRestore(enabled = false, cookies) {
   useEffect(() => {
     if (enabled && cookies) {
-      setCookies(cookies);
+      console.log("Cookies data:", cookies);
+
+      const list = cookies.reduce((result, item) => {
+        console.log("Cookie entry:", item);
+
+        item.cookies.forEach((cookie) => {
+          result.push({
+            ...cookie,
+            url: item.url,
+          });
+        });
+
+        return result;
+      }, []);
+
+      console.log("Cookies list:", list);
+
+      setCookies(list);
     }
   }, [enabled, cookies]);
 }
