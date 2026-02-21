0a7d8fc15d2534334f1b649fe5d4c4d8e870ef74
Sadiq Salau <sadiqsalau888@gmail.com>
2026-02-21 09:56:27 +0100

    feat: Included downloadFile function in bundled utils

--- apps/purrfect-farmer/src/utils/bundle.js
+++ apps/purrfect-farmer/src/utils/bundle.js
@@ -1,9 +1,10 @@
-import { getCookies, parseHTML, setCookies } from ".";
+import { downloadFile, getCookies, parseHTML, setCookies } from ".";
 
 import sharedUtils from "@purrfect/shared/utils/bundle.js";
 
 export default {
   ...sharedUtils,
+  downloadFile,
   getCookies,
   setCookies,
   parseHTML,
