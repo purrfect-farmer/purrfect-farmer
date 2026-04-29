e2ea227ff60ce24309d4869aa0552256fccfcf7e
Sadiq Salau <sadiqsalau888@gmail.com>
2026-04-29 05:44:16 +0100

    feat: Updated Spider link

--- apps/purrfect-farmer/src/components/SpiderKeyInput.jsx
+++ apps/purrfect-farmer/src/components/SpiderKeyInput.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import Input from "./Input";
-import useAppContext from "@/hooks/useAppContext";
-import { cn } from "@/utils";
 import Container from "./Container";
+import Input from "./Input";
+import { cn } from "@/utils";
+import useAppContext from "@/hooks/useAppContext";
+import { useState } from "react";
 
 export default function SpiderKeyInput() {
   const { sharedSettings, dispatchAndConfigureSharedSettings } =
@@ -30,7 +30,7 @@ export default function SpiderKeyInput() {
         <button
           className={cn(
             "px-4 bg-orange-500 text-white rounded-lg hover:bg-orange-600",
-            "shrink-0 rounded-xl font-bold"
+            "shrink-0 rounded-xl font-bold",
           )}
           onClick={handleSave}
         >
@@ -40,7 +40,7 @@ export default function SpiderKeyInput() {
       <p className="text-neutral-500 dark:text-neutral-400 text-center">
         Get your Spider API Key from the{" "}
         <a
-          href="https://t.me/S_PIDERBot?start=1147265290"
+          href="https://t.me/S_PIDERRBot?start=1147265290"
           target="_blank"
           rel="noopener noreferrer"
           className="text-orange-500"
