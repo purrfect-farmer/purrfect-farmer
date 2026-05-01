c6999d05cb8149479220f3b05fbd257c6aa3a747
Sadiq Salau <sadiqsalau888@gmail.com>
2026-05-01 05:38:02 +0100

    feat: ATF Farmer - added original referral link

--- packages/shared/farmers/ATFFarmer.js
+++ packages/shared/farmers/ATFFarmer.js
@@ -16,6 +16,7 @@ export default class ATFFarmer extends BaseFarmer {
   static host = "atfminers.asloni.online";
   static domains = ["atfminers.asloni.online"];
   static telegramLink = "https://t.me/ATF_AIRDROP_bot";
+  static originalTelegramLink = "https://t.me/ATF_AIRDROP_bot?start=8519036234";
   static path = "/miner/index.html";
   static singleton = true;
   static cacheAuth = false;
@@ -35,6 +36,11 @@ export default class ATFFarmer extends BaseFarmer {
   /** Get Referral Link */
   getReferralLink() {
     return `https://t.me/ATF_AIRDROP_bot`;
+  }
+
+  /** Get Referral Link */
+  getOriginalReferralLink() {
+    return `https://t.me/ATF_AIRDROP_bot?start=${this.getUserId()}`;
   }
 
   configureApi() {
