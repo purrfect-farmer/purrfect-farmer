6d976ae2e0b8bde390f66e117e9388f96c624197
Sadiq Salau <sadiqsalau888@gmail.com>
2026-05-13 07:04:24 +0100

    feat: Allow null on status column

--- apps/purrfect-fly/db/migrations/20260513050035-add-status-to-farmers-table.js
+++ apps/purrfect-fly/db/migrations/20260513050035-add-status-to-farmers-table.js
@@ -3,21 +3,13 @@
  * @param {import("sequelize")} Sequelize
  */
 export async function up(queryInterface, Sequelize) {
-  const table = await queryInterface.describeTable("Farmers");
-
-  if (table.active) {
-    await queryInterface.removeColumn("Farmers", "active");
-  }
-  if (table.isBanned) {
-    await queryInterface.removeColumn("Farmers", "isBanned");
-  }
-  if (!table.status) {
-    await queryInterface.addColumn("Farmers", "status", {
-      type: Sequelize.STRING,
-      defaultValue: "active",
-      allowNull: false,
-    });
-  }
+  await queryInterface.removeColumn("Farmers", "active");
+  await queryInterface.removeColumn("Farmers", "isBanned");
+  await queryInterface.addColumn("Farmers", "status", {
+    type: Sequelize.STRING,
+    defaultValue: "active",
+    allowNull: true,
+  });
 }
 
 /**
@@ -25,23 +17,15 @@ export async function up(queryInterface, Sequelize) {
  * @param {import("sequelize")} Sequelize
  */
 export async function down(queryInterface, Sequelize) {
-  const table = await queryInterface.describeTable("Farmers");
-
-  if (table.status) {
-    await queryInterface.removeColumn("Farmers", "status");
-  }
-  if (!table.active) {
-    await queryInterface.addColumn("Farmers", "active", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: true,
-      allowNull: true,
-    });
-  }
-  if (!table.isBanned) {
-    await queryInterface.addColumn("Farmers", "isBanned", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: true,
-    });
-  }
+  await queryInterface.removeColumn("Farmers", "status");
+  await queryInterface.addColumn("Farmers", "active", {
+    type: Sequelize.BOOLEAN,
+    defaultValue: true,
+    allowNull: true,
+  });
+  await queryInterface.addColumn("Farmers", "isBanned", {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: true,
+  });
 }
