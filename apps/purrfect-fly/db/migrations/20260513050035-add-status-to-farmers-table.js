49919a3c9622cf9873388082a72ed71635aaa035
Sadiq Salau <sadiqsalau888@gmail.com>
2026-05-13 06:56:46 +0100

    feat: Guard columns

--- apps/purrfect-fly/db/migrations/20260513050035-add-status-to-farmers-table.js
+++ apps/purrfect-fly/db/migrations/20260513050035-add-status-to-farmers-table.js
@@ -3,13 +3,21 @@
  * @param {import("sequelize")} Sequelize
  */
 export async function up(queryInterface, Sequelize) {
-  await queryInterface.removeColumn("Farmers", "active");
-  await queryInterface.removeColumn("Farmers", "isBanned");
-  await queryInterface.addColumn("Farmers", "status", {
-    type: Sequelize.STRING,
-    defaultValue: "active",
-    allowNull: false,
-  });
+  const table = await queryInterface.describeTable("Farmers");
+
+  if (table.active) {
+    await queryInterface.removeColumn("Farmers", "active");
+  }
+  if (table.isBanned) {
+    await queryInterface.removeColumn("Farmers", "isBanned");
+  }
+  if (!table.status) {
+    await queryInterface.addColumn("Farmers", "status", {
+      type: Sequelize.STRING,
+      defaultValue: "active",
+      allowNull: false,
+    });
+  }
 }
 
 /**
@@ -17,15 +25,23 @@ export async function up(queryInterface, Sequelize) {
  * @param {import("sequelize")} Sequelize
  */
 export async function down(queryInterface, Sequelize) {
-  await queryInterface.removeColumn("Farmers", "status");
-  await queryInterface.addColumn("Farmers", "active", {
-    type: Sequelize.BOOLEAN,
-    defaultValue: true,
-    allowNull: true,
-  });
-  await queryInterface.addColumn("Farmers", "isBanned", {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-    allowNull: true,
-  });
+  const table = await queryInterface.describeTable("Farmers");
+
+  if (table.status) {
+    await queryInterface.removeColumn("Farmers", "status");
+  }
+  if (!table.active) {
+    await queryInterface.addColumn("Farmers", "active", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: true,
+    });
+  }
+  if (!table.isBanned) {
+    await queryInterface.addColumn("Farmers", "isBanned", {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: true,
+    });
+  }
 }
