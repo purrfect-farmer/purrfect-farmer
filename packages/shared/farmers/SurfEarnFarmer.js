8d45677617a33da93332e30aac959532594fc777
Sadiq Salau <sadiqsalau888@gmail.com>
2026-07-20 03:57:15 +0100

    feat: Surf Earn Farmer - Added Wheel / Treasury game

--- packages/shared/farmers/SurfEarnFarmer.js
+++ packages/shared/farmers/SurfEarnFarmer.js
@@ -55,9 +55,37 @@ export default class SurfEarnFarmer extends BaseFarmer {
       .then((res) => res.data);
   }
 
+  async getWheel() {
+    return this.api
+      .postForm("https://surf-earn.top/load/games/wheel")
+      .then((res) => res.data);
+  }
+
+  async getTreasury() {
+    return this.api
+      .postForm("https://surf-earn.top/load/games/treasury")
+      .then((res) => res.data);
+  }
+
   async claimPendingFriendsIncome() {
     return this.api
       .postForm("https://surf-earn.top/friends/claimPendingFriendsIncome")
+      .then((res) => res.data);
+  }
+
+  async getWheelSpinForAd(data) {
+    return this.api
+      .postForm("https://surf-earn.top/games/wheel/getSpinForAd", data, {
+        headers: API_HEADERS,
+      })
+      .then((res) => res.data);
+  }
+
+  async getTreasuryKeyForAd(data) {
+    return this.api
+      .postForm("https://surf-earn.top/games/treasury/getKeyForAd", data, {
+        headers: API_HEADERS,
+      })
       .then((res) => res.data);
   }
 
@@ -98,6 +126,28 @@ export default class SurfEarnFarmer extends BaseFarmer {
       .then((res) => res.data);
   }
 
+  async startWheelRotation(multiplier) {
+    return this.api
+      .postForm(
+        "https://surf-earn.top/games/wheel/startRotation",
+        { multiplier },
+        { headers: API_HEADERS },
+      )
+      .then((res) => res.data);
+  }
+
+  async endWheelRotation() {
+    return this.api
+      .postForm("https://surf-earn.top/games/wheel/endRotation")
+      .then((res) => res.data);
+  }
+
+  async openTreasuryChest() {
+    return this.api
+      .postForm("https://surf-earn.top/games/treasury/openChest")
+      .then((res) => res.data);
+  }
+
   capitalizeFirstLetter(str) {
     if (!str) return "";
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -123,6 +173,10 @@ export default class SurfEarnFarmer extends BaseFarmer {
     const nonce = this.generateNonce();
 
     return [validationHash, phpSessionId, timestamp, nonce].join(":");
+  }
+
+  nodeTextContentToNumber(node) {
+    return parseFloat(node.textContent.trim().replaceAll(/[^\d]+/g, ""));
   }
 
   generateNonce(nonceLength = 32) {
@@ -153,7 +207,9 @@ export default class SurfEarnFarmer extends BaseFarmer {
 
   /** Process Farmer */
   async process() {
-    /* Daily Tasks */
+    /* Tasks */
+    await this.executeTask("Treasury", () => this.playTreasuryGame());
+    await this.executeTask("Wheel", () => this.playWheelGame());
     await this.executeTask("Friends Reward", () => this.collectFriendsReward());
     await this.executeTask("Tasks", () => this.completeTasks());
   }
@@ -209,6 +265,31 @@ export default class SurfEarnFarmer extends BaseFarmer {
     return { pendingCoins };
   }
 
+  async extractWheelData(wheelHtml) {
+    const dom = await this.utils.parseHTML(wheelHtml);
+
+    const spinsNode = dom.getElementById("spins");
+    const adsWatchedTodayNode = dom.getElementById("adsWatchedToday");
+
+    const spins = this.nodeTextContentToNumber(spinsNode);
+    const adsWatchedToday = this.nodeTextContentToNumber(adsWatchedTodayNode);
+
+    return { spins, adsWatchedToday };
+  }
+
+  async extractTreasuryData(treasuryHtml) {
+    const dom = await this.utils.parseHTML(treasuryHtml);
+
+    const keysNode = dom.getElementById("keys");
+    const adsWatchedTodayNode = dom.getElementById("adsWatchedToday");
+
+    const keys = this.nodeTextContentToNumber(keysNode);
+    const adsWatchedToday = this.nodeTextContentToNumber(adsWatchedTodayNode);
+
+    return { keys, adsWatchedToday };
+  }
+
+  /** Complete verification code */
   async completeVerificationCode() {
     const verificationCodeStatus = await this.sendVerificationCode();
 
@@ -254,13 +335,16 @@ export default class SurfEarnFarmer extends BaseFarmer {
     }
   }
 
+  /** Complete tasks */
   async completeTasks() {
     const earningsHtml = await this.getEarnings();
     const { tasks } = await this.extractEarnings(earningsHtml);
+    const availableTasks = tasks.filter((item) => item.category !== "promo");
 
     this.debugger.log("All tasks:", tasks);
+    this.debugger.log("Available tasks:", availableTasks);
 
-    for (const item of tasks) {
+    for (const item of availableTasks) {
       if (this.signal.aborted) return;
 
       this.debugger.log("Current task:", item);
@@ -318,6 +402,7 @@ export default class SurfEarnFarmer extends BaseFarmer {
     }
   }
 
+  /** Collect friends reward */
   async collectFriendsReward() {
     const friendsHtml = await this.getFriends();
     const { pendingCoins } = await this.extractFriendsData(friendsHtml);
@@ -326,6 +411,123 @@ export default class SurfEarnFarmer extends BaseFarmer {
       this.logger.keyValue("Pending coins", pendingCoins);
       await this.claimPendingFriendsIncome();
       this.logger.success("Claimed friends reward!");
+    }
+  }
+
+  /** Play Wheel Game */
+  async playWheelGame() {
+    // Powers: x1, x5, x10, x50, x100
+    const multipliers = [1, 5, 10, 50, 100].sort((a, b) => b - a);
+    const wheelHtml = await this.getWheel();
+    let { spins, adsWatchedToday } = await this.extractWheelData(wheelHtml);
+
+    /** Watch ads for spin */
+    for (let i = adsWatchedToday; i < 30; i++) {
+      if (this.signal.aborted) return;
+
+      this.logger.info(`Watching ad for spin... (${i + 1})`);
+      await this.completeVerificationCode();
+      const info = await this.getWheelSpinForAd();
+      const validationHash = info["validation_hash"];
+
+      this.debugger.info("Spin for ad info:", info);
+
+      /** Delay */
+      await this.utils.delayForSeconds(10);
+
+      const result = await this.getWheelSpinForAd({
+        data: await this.getSignature(validationHash),
+      });
+
+      this.debugger.info("Spin for ad result:", result);
+
+      /** Log successful claim */
+      if (result.icon === "success") {
+        spins = parseFloat(result.spins);
+        this.logger.success(`Claimed spin (${result.spins})`);
+      }
+
+      await this.utils.delayForSeconds(5);
+    }
+
+    /** Rotate the wheel */
+    while (true) {
+      if (this.signal.aborted) return;
+
+      /* Multiplier to use */
+      const multiplierToUse = multipliers.find((item) => spins >= item);
+
+      if (!multiplierToUse) return;
+
+      this.logger.keyValue("Multiplier", `x${multiplierToUse}`);
+
+      /** Get spin start result */
+      const startResult = await this.startWheelRotation(multiplierToUse);
+      this.debugger.log("Wheel Spin Start Result:", startResult);
+
+      /** Delay */
+      await this.utils.delayForSeconds(5);
+
+      /** Get spin end result */
+      const endResult = await this.endWheelRotation();
+      this.debugger.log("Wheel Spin End Result:", endResult);
+
+      /** Update spins */
+      spins = parseFloat(endResult.spins);
+
+      /** Add little delay */
+      await this.utils.delayForSeconds(2);
+    }
+  }
+
+  /** Play Treasury Game */
+  async playTreasuryGame() {
+    const treasuryHtml = await this.getTreasury();
+    let { keys, adsWatchedToday } =
+      await this.extractTreasuryData(treasuryHtml);
+
+    /** Watch ads for key */
+    for (let i = adsWatchedToday; i < 30; i++) {
+      if (this.signal.aborted) return;
+
+      this.logger.info(`Watching ad for key... (${i + 1})`);
+      await this.completeVerificationCode();
+      const info = await this.getTreasuryKeyForAd();
+      const validationHash = info["validation_hash"];
+
+      this.debugger.info("Key for ad info:", info);
+
+      /** Delay */
+      await this.utils.delayForSeconds(10);
+
+      /** Get result for key */
+      const result = await this.getTreasuryKeyForAd({
+        data: await this.getSignature(validationHash),
+      });
+
+      this.debugger.info("Key for ad result:", result);
+
+      /** Log successful claim */
+      if (result.icon === "success") {
+        keys = parseFloat(result.keys);
+        this.logger.success(`Claimed key (${result.keys})`);
+      }
+
+      await this.utils.delayForSeconds(5);
+    }
+
+    /** Open the chest */
+    while (keys > 0) {
+      if (this.signal.aborted) return;
+
+      /** Open chest result */
+      const result = await this.openTreasuryChest();
+      this.debugger.log("Treasury Chest Result:", result);
+
+      /** Update keys */
+      keys = parseFloat(result.keys);
+
+      await this.utils.delayForSeconds(5);
     }
   }
 }
