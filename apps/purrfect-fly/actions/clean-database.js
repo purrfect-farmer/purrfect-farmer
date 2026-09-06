97431f5d5f3b4cdb65166b1dc908cf0c6a877906
Sadiq Salau <sadiqsalau888@gmail.com>
2026-09-06 06:59:50 +0100

    feat: Remove duplicate farmers

--- apps/purrfect-fly/actions/clean-database.js
+++ apps/purrfect-fly/actions/clean-database.js
@@ -1,14 +1,38 @@
-import farmers from "../farmers/index.js";
 import db from "../db/models/index.js";
 
 /** Clean Database */
 async function cleanDatabase() {
+  /* Get all farmer IDs */
   const farmerIds = Object.keys(farmers);
 
+  /* Log farmer IDs */
   console.log("Farmer IDs:", farmerIds);
+
+  /* Destroy farmers not in farmer IDs */
   await db.Farmer.destroy({
     where: {
       farmer: { [db.Sequelize.Op.notIn]: farmerIds },
+    },
+  });
+
+  /* Get all active farmers */
+  const farmers = await db.Farmer.findAll();
+
+  /* Find duplicate farmers */
+  const duplicateFarmers = farmers.filter(
+    (farmer, index, self) =>
+      self.findIndex(
+        (t) => t.accountId === farmer.accountId && t.farmer === farmer.farmer,
+      ) !== index,
+  );
+
+  /* Log duplicate farmers */
+  console.log("Duplicate Farmers:", duplicateFarmers);
+
+  /* Destroy duplicate farmers */
+  await db.Farmer.destroy({
+    where: {
+      id: { [db.Sequelize.Op.in]: duplicateFarmers.map((f) => f.id) },
     },
   });
 }
