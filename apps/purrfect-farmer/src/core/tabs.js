12d0f95a396720e4a73d1a1a696fbd8f5413bf2d
Sadiq Salau <sadiqsalau888@gmail.com>
2026-09-20 07:14:19 +0100

    feat: Dropped singleton from autos

--- apps/purrfect-farmer/src/core/tabs.js
+++ apps/purrfect-farmer/src/core/tabs.js
@@ -93,7 +93,6 @@ export const utils = [
         title: config.title,
         icon: config.icon,
         component: createElement(Auto, { config }),
-        singleton: true,
       })),
     ],
   },
